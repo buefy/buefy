ef936ccacd4394590b6f45a8ccbb4376f6c0b1d1
Joshua Bartlett <joshuabartlett@gmail.com>
2017-07-27 22:19:31 -0500

    Add requested features from GitHub discussion

--- src/components/datepicker/DatepickerTableRow.vue
+++ src/components/datepicker/DatepickerTableRow.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="datepicker-row">
-    <div v-for="day in week"
-        :key="day.getDate()"
-        class="datepicker-cell"
-        :class="classObject(day)"
-        @click="emitChosenDate(day)">
-          {{day.getDate()}}
+    <div
+      v-for="day in week"
+      :key="day.getDate()"
+      class="datepicker-cell"
+      :class="classObject(day)"
+      @click="emitChosenDate(day)">
+        {{day.getDate()}}
     </div>
 </div>
-
 </template>
 
 <script>
@@ -26,29 +26,57 @@ export default {
         month: {
             type: Number,
             required: true
-        }
+        },
+        earliestDate: Date,
+        latestDate: Date
     },
     methods: {
+        /*
+         * Check that selected day is within earliest/latest params and
+         * is within this month
+         */
+        selectableDate(day) {
+            const validity = []
+
+            if (this.earliestDate) {
+                validity.push(day >= this.earliestDate)
+            }
+
+            if (this.latestDate) {
+                validity.push(day <= this.latestDate)
+            }
+
+            validity.push(day.getMonth() === this.month);
+
+            return !validity.includes(false)
+        },
+
+        /*
+         * Emit select event with chosen date as payload
+         */
         emitChosenDate(day) {
-            if (day.getMonth() === this.month) {
+            if (this.selectableDate(day)) {
                 this.$emit('select', day)
             }
         },
+
+        /*
+         * Build classObject for cell using validations
+         */
         classObject(day) {
             function dateMatch(dateOne, dateTwo) {
-              return (dateOne.getDate() === dateTwo.getDate() &&
-                  dateOne.getFullYear() === dateTwo.getFullYear() &&
-                  dateOne.getMonth() === dateTwo.getMonth())
+                return (dateOne.getDate() === dateTwo.getDate() &&
+                    dateOne.getFullYear() === dateTwo.getFullYear() &&
+                    dateOne.getMonth() === dateTwo.getMonth())
             }
 
             return {
                 selected: dateMatch(day, this.selectedDate),
                 today: dateMatch(day, new Date()),
-                selectable: (day.getMonth() === this.month),
-                unselectable: (day.getMonth() !== this.month)
+                selectable: this.selectableDate(day),
+                unselectable: !this.selectableDate(day)
             }
         }
     }
 }
-
 </script>
