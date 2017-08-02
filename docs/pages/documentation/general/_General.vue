d6a93036482eddc80fab52921907bedf950dd368
David Graham <prograhammer@gmail.com>
2017-08-02 16:14:21 -0500

    add card component with collapsible

--- docs/pages/documentation/general/_General.vue
+++ docs/pages/documentation/general/_General.vue
@@ -2,6 +2,7 @@
     <div class="nav-left">
         <router-link to="layout" class="nav-item is-tab">Layout</router-link>
         <router-link to="icon" class="nav-item is-tab">Icon</router-link>
+        <router-link to="card" class="nav-item is-tab">Card</router-link>
         <router-link to="dropdown" class="nav-item is-tab">Dropdown</router-link>
         <router-link to="menu" class="nav-item is-tab">Menu</router-link>
         <router-link to="modal" class="nav-item is-tab">Modal</router-link>
