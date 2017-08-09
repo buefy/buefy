02ab3dd6ba1ce8de545ab24b5c406b3e0e7c5560
Joshua Bartlett <joshuabartlett@gmail.com>
2017-08-09 13:15:09 -0500

    Move Datepicker Input into Dropdown for cleaner code

--- src/components/datepicker/Datepicker.vue
+++ src/components/datepicker/Datepicker.vue
@@ -1,110 +1,98 @@
 <template>
   <div class="control datepicker" :class="{size, 'is-expanded': expanded}">
-      <b-input :value="formatValue(value)"
-          @keydown.native="preventTyping"
-          :placeholder="placeholder"
-          ref="input"
-          :size="size"
-          :icon="icon"
-          :icon-pack="iconPack"
-          :loading="loading"
-          :class="[type]"
-          autocomplete="off"
-          v-bind="$attrs"
-          @focus="focused"
-          @blur="blurred">
-      </b-input>
-
-      <transition name="fade">
-          <div class="dropdown">
-
-              <transition name="fade">
-                  <div v-show="isActive" class="background"></div>
-              </transition>
-
-              <div class="dropdown-menu"
-                  :class="{ 'is-opened-top': !isListInViewportVertically }"
-                  v-show="isActive"
-                  ref="dropdown">
-                  <div class="dropdown-content">
-                      <div class="datepicker-container">
-                          <div class="level is-marginless">
-                              <div class="level-item">
-                                  <h4 class="has-text-centered datepicker-month level-item">
-                                      <span
-                                          @click="decrementMonth"
-                                          @keydown.enter="decrementMonth"
-                                          @keydown.space.prevent="decrementMonth"
-                                          class="datepicker-decrement"
-                                          role="button"
-                                          tabindex="0"
-                                          aria-label="Decrement Month">
-                                              <b-icon icon="chevron_left"
-                                                  both
-                                                  type="is-primary is-clickable"
-                                                  v-show="!isFirstMonth">
-                                              </b-icon>
-                                      </span>
-                                      <b-field grouped>
-                                          <b-select v-model="focusedDateData.month" aria-label="Select Month">
-                                              <option v-for="(month, index) in Object.values(monthNames)" :value="index" :key="month">
-                                                {{month}}
-                                              </option>
-                                          </b-select>
-                                          <b-select v-model="focusedDateData.year" aria-label="Select Year">
-                                              <option v-for="year in listOfYears" :value="year" :key="year">
-                                                {{year}}
-                                              </option>
-                                          </b-select>
-                                      </b-field>
-                                      <span
-                                          @click="incrementMonth"
-                                          @keydown.enter="incrementMonth"
-                                          @keydown.space.prevent="incrementMonth"
-                                          class="datepicker-increment"
-                                          role="button"
-                                          tabindex="0"
-                                          aria-label="Increment Month">
-                                              <b-icon icon="chevron_right"
-                                                  both
-                                                  type="is-primary is-clickable"
-                                                  v-show="!isLastMonth">
-                                              </b-icon>
-                                      </span>
-                                  </h4>
-                              </div>
-                          </div>
-                          <b-datepicker-table
-                            v-model="dateSelected"
-                            :day-names="dayNames"
-                            :month-names="monthNames"
-                            :earliest-date="earliestDate"
-                            :latest-date="latestDate"
-                            :focused="focusedDateData"
-                            @close="isActive = false">
-                          </b-datepicker-table>
-                          <div class="columns">
-                              <div class="column has-text-centered">
-                                  <a role="button" @click="updateSelectedDate(new Date())">
-                                      <slot name="selectToday"></slot>
-                                  </a>
-                              </div>
-                              <div class="column has-text-centered">
-                                  <a role="button" @click="updateSelectedDate(null)">
-                                      <slot name="clearSelection"></slot>
-                                  </a>
-                              </div>
-                              <div class="column has-text-centered">
-                                  <a role="button" @click="isActive=false">
-                                      <slot name="close"></slot>
-                                  </a>
-                              </div>
-                          </div>
-                    </div>
+      <b-dropdown ref="dropdown">
+          <b-input :value="formatValue(value)"
+              @keydown.native="preventTyping"
+              :placeholder="placeholder"
+              ref="input"
+              :size="size"
+              :icon="icon"
+              :icon-pack="iconPack"
+              :loading="loading"
+              :class="[type]"
+              autocomplete="off"
+              slot="trigger"
+              v-bind="$attrs"
+              @focus="focused"
+              @blur="blurred">
+          </b-input>
+          <b-dropdown-item custom>
+              <div class="datepicker-container">
+                  <div class="level is-marginless">
+                      <div class="level-item">
+                          <h4 class="has-text-centered datepicker-month level-item">
+                              <span
+                                  @click="decrementMonth"
+                                  @keydown.enter="decrementMonth"
+                                  @keydown.space.prevent="decrementMonth"
+                                  class="datepicker-decrement"
+                                  role="button"
+                                  tabindex="0"
+                                  aria-label="Decrement Month">
+                                      <b-icon icon="chevron_left"
+                                          both
+                                          type="is-primary is-clickable"
+                                          v-show="!isFirstMonth">
+                                      </b-icon>
+                              </span>
+                              <b-field grouped>
+                                  <b-select v-model="focusedDateData.month" aria-label="Select Month">
+                                      <option v-for="(month, index) in Object.values(monthNames)" :value="index" :key="month">
+                                        {{month}}
+                                      </option>
+                                  </b-select>
+                                  <b-select v-model="focusedDateData.year" aria-label="Select Year">
+                                      <option v-for="year in listOfYears" :value="year" :key="year">
+                                        {{year}}
+                                      </option>
+                                  </b-select>
+                              </b-field>
+                              <span
+                                  @click="incrementMonth"
+                                  @keydown.enter="incrementMonth"
+                                  @keydown.space.prevent="incrementMonth"
+                                  class="datepicker-increment"
+                                  role="button"
+                                  tabindex="0"
+                                  aria-label="Increment Month">
+                                      <b-icon icon="chevron_right"
+                                          both
+                                          type="is-primary is-clickable"
+                                          v-show="!isLastMonth">
+                                      </b-icon>
+                              </span>
+                          </h4>
+                      </div>
+                  </div>
+                  <b-datepicker-table
+                      v-model="dateSelected"
+                      :day-names="dayNames"
+                      :month-names="monthNames"
+                      :earliest-date="earliestDate"
+                      :latest-date="latestDate"
+                      :focused="focusedDateData"
+                      @close="$refs.dropdown.isActive = false">
+                  </b-datepicker-table>
+                  <div class="columns">
+                      <div class="column has-text-centered">
+                          <a role="button" @click="updateSelectedDate(new Date())">
+                              <slot name="selectToday"></slot>
+                          </a>
+                      </div>
+                      <div class="column has-text-centered">
+                          <a role="button" @click="updateSelectedDate(null)">
+                              <slot name="clearSelection"></slot>
+                          </a>
+                      </div>
+                      <div class="column has-text-centered">
+                          <a role="button" @click="$refs.dropdown.isActive=false">
+                              <slot name="close"></slot>
+                          </a>
+                      </div>
                   </div>
               </div>
-          </div>
-      </transition>
+          </b-dropdown-item>
+      </b-dropdown>
   </div>
 </template>
 
@@ -114,6 +102,7 @@ import Input from '../input'
 import Field from '../field'
 import Select from '../select'
 import Icon from '../icon'
+import { Dropdown, DropdownItem } from '../dropdown'
 import DatepickerTable from './DatepickerTable'
 export default {
     name: 'bDatepicker',
@@ -124,7 +113,9 @@ export default {
         [Input.name]: Input,
         [Field.name]: Field,
         [Select.name]: Select,
-        [Icon.name]: Icon
+        [Icon.name]: Icon,
+        [Dropdown.name]: Dropdown,
+        [DropdownItem.name]: DropdownItem
     },
     props: {
         value: [Date, String],
@@ -186,9 +177,6 @@ export default {
                 month: focusedDate.getMonth(),
                 year: focusedDate.getFullYear()
             },
-            hovered: null,
-            isActive: false,
-            isListInViewportVertically: true,
             _elementRef: 'input',
             _isDatepicker: true
         }
@@ -238,7 +226,7 @@ export default {
               ? this.formatValue(val) : val
 
             this.$emit('input', date)
-            this.isActive = false
+            this.$refs.dropdown.isActive = false
         }
     },
     methods: {
@@ -263,22 +251,6 @@ export default {
             } else {
                 return null
             }
-        },
-
-        /**
-         * Calculate if the dropdown is vertically visible when activated,
-         * otherwise it is opened upwards.
-         */
-        calcDropdownInViewportVertical() {
-            this.$nextTick(() => {
-                const rect = this.$refs.dropdown.getBoundingClientRect()
-
-                this.isListInViewportVertically = (
-                    rect.top >= 0 &&
-                    rect.bottom <= (window.innerHeight ||
-                        document.documentElement.clientHeight)
-                )
-            })
         },
 
         /*
@@ -323,7 +295,8 @@ export default {
          * Handle focus event
          */
         focused(event) {
-            this.isActive = true
+            // console.log(this.$refs.dropdown)
+            // this.$refs.dropdown.isActive = true
             this.$emit('focus', event)
         },
 
@@ -333,7 +306,7 @@ export default {
         blurred(event) {
             this.$emit('blur', event)
             if (this.closeOnBlur) {
-                this.isActive = false
+                this.$refs.dropdown.isActive = false
             }
         },
 
