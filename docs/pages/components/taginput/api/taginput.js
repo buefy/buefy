3dc7e161bbe0ba34e1d40cc57135242cfcd50afe
Adrien Fourel <ad.fourel@gmail.com>
2018-02-14 16:07:47 +0100

    TagInput | fix doc for tag type

--- docs/pages/components/taginput/api/taginput.js
+++ docs/pages/components/taginput/api/taginput.js
@@ -30,7 +30,7 @@ export default [
                     <code>is-dark</code>, <code>is-primary</code>, <code>is-info</code>, <code>is-success</code>,
                     <code>is-warning</code>, <code>is-danger</code>,
                     and any other colors you've set in the <code>$colors</code> list on Sass`,
-                default: '<code>is-primary</code>'
+                default: '<code>is-light</code>'
             },
             {
                 name: '<code>size</code>',
