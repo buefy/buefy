e4b99866b9304c876acb268ecb0e175173df9778
Walter Tommasi <tommsi20@gmail.com>
2018-03-15 20:07:31 +0100

    Fix message name snackbar example

--- docs/pages/components/snackbar/examples/ExSimple.vue
+++ docs/pages/components/snackbar/examples/ExSimple.vue
@@ -22,7 +22,7 @@
             },
             warning() {
                 this.$snackbar.open({
-                    message: 'Yellow button and positioned top-left',
+                    message: 'Yellow button and positioned on top',
                     type: 'is-warning',
                     position: 'is-top',
                     actionText: 'Retry',
