5061b6501708c7e48935a5f98f7f20596b502c49
Walter Tommasi <tommsi20@gmail.com>
2018-03-20 00:14:31 +0100

    Update detailed row table docs #508

--- docs/pages/components/table/examples/ExDetailedRow.vue
+++ docs/pages/components/table/examples/ExDetailedRow.vue
@@ -1,29 +1,37 @@
 <template>
     <b-table
-        :data="tableDataSimple"
+        :data="data"
+        paginated
+        per-page="5"
         :opened-detailed="defaultOpenedDetails"
         detailed
         detail-key="id"
-        @details-open="(row, index) => $toast.open(`Expanded ${row.first_name}`)">
+        @details-open="(row, index) => $toast.open(`Expanded ${row.user.first_name}`)"
+    >
 
         <template slot-scope="props">
-            <b-table-column label="ID" width="40"  numeric>
+            <b-table-column field="id" label="ID" width="40" numeric>
                 {{ props.row.id }}
             </b-table-column>
 
-            <b-table-column label="First Name">
-                {{ props.row.first_name }}
+            <b-table-column field="user.first_name" label="First Name" sortable>
+                {{ props.row.user.first_name }}
             </b-table-column>
 
-            <b-table-column label="Last Name">
-                {{ props.row.last_name }}
+            <b-table-column field="user.last_name" label="Last Name" sortable>
+                {{ props.row.user.last_name }}
             </b-table-column>
 
-            <b-table-column label="Date" centered>
-                {{ new Date(props.row.date).toLocaleDateString() }}
+            <b-table-column field="date" label="Date" sortable centered>
+                <span class="tag is-success">
+                    {{ new Date(props.row.date).toLocaleDateString() }}
+                </span>
             </b-table-column>
 
             <b-table-column label="Gender">
+                <b-icon pack="fa"
+                    :icon="props.row.gender === 'Male' ? 'mars' : 'venus'">
+                </b-icon>
                 {{ props.row.gender }}
             </b-table-column>
         </template>
@@ -38,8 +46,8 @@
                 <div class="media-content">
                     <div class="content">
                         <p>
-                            <strong>{{ props.row.first_name }} {{ props.row.last_name }}</strong>
-                            <small>@{{ props.row.first_name }}</small>
+                            <strong>{{ props.row.user.first_name }} {{ props.row.user.last_name }}</strong>
+                            <small>@{{ props.row.user.first_name }}</small>
                             <small>31m</small>
                             <br>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -54,18 +62,12 @@
 </template>
 
 <script>
+    const data = require('@/assets/data_test.json')
+
     export default {
         data() {
-            const tableDataSimple = [
-                { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016/10/15 13:43:27', 'gender': 'Male' },
-                { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016/12/15 06:00:53', 'gender': 'Male' },
-                { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016/04/26 06:26:28', 'gender': 'Female' },
-                { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016/04/10 10:28:46', 'gender': 'Male' },
-                { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016/12/06 14:38:38', 'gender': 'Female' }
-            ]
-
             return {
-                tableDataSimple,
+                data,
                 defaultOpenedDetails: [1]
             }
         }
