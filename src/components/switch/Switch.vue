buefy/buefy

commit f9aacfa9b259129db5148182e90f7cd754235db3

Prevent click event to be emitted twice from switch

src/components/switch/Switch.vue
@@ -5,6 +5,7 @@
         ref="label"
         :disabled="disabled"
         :tabindex="disabled ? false : 0"
+        @click.prevent="newValue = !newValue"
         @keydown.prevent.enter.space="$refs.label.click()"
         @mousedown="isMouseDown = true"
         @mouseup="isMouseDown = false"

commit cb1c07c5fb7ac8ff668317efc95cee7e92074d51

fix #710 switch with true/false values - regression from #671

src/components/switch/Switch.vue
@@ -5,7 +5,6 @@
         ref="label"
         :disabled="disabled"
         :tabindex="disabled ? false : 0"
-        @click.prevent="newValue = !newValue"
         @keydown.prevent.enter.space="$refs.label.click()"
         @mousedown="isMouseDown = true"
         @mouseup="isMouseDown = false"
@@ -14,6 +13,7 @@
         <input
             v-model="newValue"
             type="checkbox"
+            @click.stop
             :name="name"
             :value="nativeValue"
             :disabled="disabled"