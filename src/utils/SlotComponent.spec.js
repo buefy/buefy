4a67c2fcfa22cc3da84ae2bf052ba552589c75a9
Walter Tommasi <tommsi20@gmail.com>
2018-03-26 00:19:31 +0200

    Update SlotComponent unit test

--- src/utils/SlotComponent.spec.js
+++ src/utils/SlotComponent.spec.js
@@ -1,11 +1,56 @@
 import { shallow } from '@vue/test-utils'
 import BSlotComponent from '@utils/SlotComponent'
+import Vue from 'vue'
 
 describe('BSlotComponent', () => {
     it('is called', () => {
-        const wrapper = shallow(BSlotComponent)
+        const wrapper = shallow(BSlotComponent, {
+            propsData: {
+                component: {}
+            }
+        })
 
         expect(wrapper.name()).toBe('BSlotComponent')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('default render', () => {
+
+        const slot = 'Content'
+        const component = shallow(Vue, {
+            slots: {
+                default: slot
+            }
+        })
+
+        const wrapper = shallow(BSlotComponent, {
+            propsData: {
+                component: component.vm
+            }
+        })
+
+        expect(wrapper.html()).toBe(`<div>${slot}</div>`)
+    })
+
+    it('render', () => {
+
+        const slot = 'Content'
+        const slotName = 'header'
+        const component = shallow(Vue, {
+            slots: {
+                [slotName]: slot
+            }
+        })
+
+        const tag = 'span'
+        const wrapper = shallow(BSlotComponent, {
+            propsData: {
+                component: component.vm,
+                tag: tag,
+                name: slotName
+            }
+        })
+
+        expect(wrapper.html()).toBe(`<${tag}>${slot}</${tag}>`)
     })
 })
