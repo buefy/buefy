400f99c4c580c9e9de5183653d664b5f332e204d
Bruno Oliveira <bruno.oliveira@intelipost.com.br>
2018-03-29 16:25:03 -0300

    Add stop in click for using inside b-table with selection

--- src/components/dropdown/Dropdown.vue
+++ src/components/dropdown/Dropdown.vue
@@ -5,7 +5,7 @@
             role="button"
             ref="trigger"
             class="dropdown-trigger"
-            @click="toggle">
+            @click.stop="toggle">
             <slot name="trigger"/>
         </div>
 
