769b6b813a8516092366e3a875f9f9e19abee64d
Walter Tommasi <tommsi20@gmail.com>
2018-03-30 16:07:50 +0200

    Fix tabs header example

--- docs/pages/components/tabs/examples/ExCustomHeaders.vue
+++ docs/pages/components/tabs/examples/ExCustomHeaders.vue
@@ -2,13 +2,13 @@
     <b-tabs type="is-boxed">
         <b-tab-item>
             <template slot="header">
-                <b-icon icon="information-outline" />
+                <b-icon icon="information-outline"></b-icon>
                 <span> Issues <b-tag rounded> 3 </b-tag> </span>
             </template>
         </b-tab-item>
         <b-tab-item>
             <template slot="header">
-                <b-icon icon="source-pull" />
+                <b-icon icon="source-pull"></b-icon>
                 <span> Pull Requests <b-tag rounded> 1 </b-tag> </span>
             </template>
         </b-tab-item>
