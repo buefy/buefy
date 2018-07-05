36bbb007cd9bd59d7131c05bff3b69d0fe972216
Walter Tommasi <tommsi20@gmail.com>
2018-07-05 19:38:18 +0200

    Enable hot module reloading

--- build/dev-server.js
+++ build/dev-server.js
@@ -36,7 +36,7 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    // hotMiddleware.publish({ action: 'reload' })
     cb()
   })
 })
