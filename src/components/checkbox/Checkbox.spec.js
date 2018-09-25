49a06def9a202eca808d504f6534e054dcc3af11
nRV <contact@styro.fr>
2018-09-25 19:37:13 +0200

    test: add unit tests for Checkbox component

--- src/components/checkbox/Checkbox.spec.js
+++ src/components/checkbox/Checkbox.spec.js
@@ -1,10 +1,30 @@
 import { shallow } from '@vue/test-utils'
+import Helpers from 'mwangaben-vthelpers'
 import BCheckbox from '@components/checkbox/Checkbox'
 
+let wrapper, b
+
 describe('BCheckbox', () => {
+    beforeEach(() => {
+        wrapper = shallow(BCheckbox)
+        b = new Helpers(wrapper, expect)
+    })
+
     it('is called', () => {
-        const wrapper = shallow(BCheckbox)
         expect(wrapper.name()).toBe('BCheckbox')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('has an input checkbox', () => {
+        b.domHas('label input[type=checkbox]')
+    })
+
+    it('emit input event with value when value change', async () => {
+        const NEW_VALUE = true
+        wrapper.vm.value = NEW_VALUE
+        await wrapper.vm.$nextTick()
+
+        expect(wrapper.vm.newValue).toBeTruthy()
+        b.emittedContains('input', NEW_VALUE)
     })
 })
