49ca9297886d7f89994382dfe182c5b3ace9651f
nRV <contact@styro.fr>
2018-09-25 19:30:46 +0200

    chore: add vthelper pkg

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
         '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
         '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
     },
+    transformIgnorePatterns: [
+        '<rootDir>/node_modules/(?!mwangaben-vthelpers)'
+    ],
     moduleNameMapper: {
         '^@components/(.*)$': '<rootDir>/src/components/$1',
         '^@utils/(.*)$': '<rootDir>/src/utils/$1'
