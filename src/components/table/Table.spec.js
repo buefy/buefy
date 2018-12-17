8a4afdf298a34d367457afc76650dc3a7c39dbb3
Carlo Beltrame <carlo@beltra.me>
2018-12-17 16:11:33 +0100

    Add a test for the customRow prop

--- src/components/table/Table.spec.js
+++ src/components/table/Table.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import BTable from '@components/table/Table'
 
 describe('BTable', () => {
@@ -6,5 +6,14 @@ describe('BTable', () => {
         const wrapper = mount(BTable)
         expect(wrapper.name()).toBe('BTable')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('allows custom row component', () => {
+        const wrapper = shallowMount(BTable, {
+            propsData: {
+                customRow: true
+            }
+        })
+        expect(wrapper.props().customRow).toBe(true)
     })
 })
