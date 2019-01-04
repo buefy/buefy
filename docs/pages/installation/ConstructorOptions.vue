beed425643a1ae5d8c30f09c6eb5a2bb088e7e2e
Steve Perry <steveperrito@gmail.com>
2019-01-04 09:29:46 -0700

    Config for individual use of components
    When using components individually, one must first import the config object to set global options. Thought that info would be useful here.

--- docs/pages/installation/ConstructorOptions.vue
+++ docs/pages/installation/ConstructorOptions.vue
@@ -3,6 +3,12 @@
         <div class="example is-paddingless">
             <CodeView :code="usage | pre" lang="javascript" expanded/>
         </div>
+        
+        <b-message type="is-warning">
+            If using components individually, set global options like this:<br>
+            <code>import config from 'buefy/src/utils/config';</code><br>
+            <code>config.defaultIconPack = 'fas'; // or any other constructor option</code>
+        </b-message>
 
         <ApiView :data="api"/>
     </div>
