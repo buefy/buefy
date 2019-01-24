3e1d4502bf659cd94f68d3e8d931efe637f41b8a
Ralph Huwiler <rhwilr@users.noreply.github.com>
2019-01-24 13:34:19 +0100

    Added an option to enable the use of the fontawesome 5 svg component (#1073)

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -1,6 +1,15 @@
 <template>
     <span class="icon" :class="[newType, size]">
-        <i :class="[newPack, newIcon, newCustomSize, customClass]"/>
+        <i
+            v-if="!useIconComponent"
+            :class="[newPack, newIcon, newCustomSize, customClass]"/>
+
+        <component
+            v-else
+            :is="useIconComponent"
+            :icon="[newPack, newIcon]"
+            :size="newCustomSize"
+            :class="[customClass]"/>
     </span>
 </template>
 
@@ -25,17 +34,9 @@
              * internal icons are always MDI.
              */
             newIcon() {
-                if (!this.both) {
-                    if (this.newPack === 'mdi') {
-                        return `${this.newPack}-${this.icon}`
-                    } else {
-                        return `fa-${this.icon}`
-                    }
-                }
-
                 return this.newPack === 'mdi'
                     ? `${this.newPack}-${this.icon}`
-                    : `fa-${this.getEquivalentIconOf(this.icon)}`
+                    : this.addFAPrefix(this.getEquivalentIconOf(this.icon))
             },
             newPack() {
                 return this.pack || config.defaultIconPack
@@ -64,26 +65,41 @@
             customSizeByPack() {
                 const defaultSize = this.newPack === 'mdi'
                     ? 'mdi-24px'
-                    : 'fa-lg'
+                    : this.addFAPrefix('lg')
                 const mediumSize = this.newPack === 'mdi'
                     ? 'mdi-36px'
-                    : 'fa-2x'
+                    : this.addFAPrefix('2x')
                 const largeSize = this.newPack === 'mdi'
                     ? 'mdi-48px'
-                    : 'fa-3x'
+                    : this.addFAPrefix('3x')
                 switch (this.size) {
                     case 'is-small': return
                     case 'is-medium': return mediumSize
                     case 'is-large': return largeSize
                     default: return defaultSize
                 }
+            },
+            useIconComponent() {
+                return config.defaultIconComponent
             }
         },
         methods: {
+            addFAPrefix(value) {
+                if (this.useIconComponent) {
+                    return value
+                }
+                return `fa-${value}`
+            },
+
             /**
              * Equivalent FA icon name of the MDI.
              */
             getEquivalentIconOf(value) {
+                // Only transform the class if the both prop is set to true
+                if (!this.both) {
+                    return value
+                }
+
                 switch (value) {
                     case 'check': return 'check'
                     case 'information': return 'info-circle'
