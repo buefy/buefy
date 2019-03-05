cd42673f4486a7e6bc76a117e5ff367a6ba8df4c
Mateus Machado Luna <mateus.m.luna@gmail.com>
2019-03-05 20:37:41 -0300

    Notifications a11y (#1264)

--- docs/pages/components/notification/examples/ExIcons.vue
+++ docs/pages/components/notification/examples/ExIcons.vue
@@ -1,18 +1,32 @@
 <template>
     <section>
-        <b-notification type="is-info" has-icon>
+        <b-notification
+            type="is-info"
+            has-icon
+            aria-close-label="Close notification">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
         </b-notification>
 
-        <b-notification type="is-success" has-icon>
+        <b-notification
+            type="is-success"
+            has-icon
+            aria-close-label="Close notification">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
         </b-notification>
 
-        <b-notification type="is-warning" has-icon>
+        <b-notification
+            type="is-warning"
+            has-icon
+            aria-close-label="Close notification"
+            role="alert">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
         </b-notification>
 
-        <b-notification type="is-danger" has-icon>
+        <b-notification
+            type="is-danger"
+            has-icon
+            aria-close-label="Close notification"
+            role="alert">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
         </b-notification>
     </section>
