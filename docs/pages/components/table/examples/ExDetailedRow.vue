7b6f9846b4544075cfc1c7f2935570f173fa599b
Mateus Machado Luna <mateus.m.luna@gmail.com>
2019-03-12 19:21:36 -0300

    Accessibility changes for table\'s internal pagination.

--- docs/pages/components/table/examples/ExDetailedRow.vue
+++ docs/pages/components/table/examples/ExDetailedRow.vue
@@ -17,7 +17,10 @@
             detail-key="id"
             @details-open="(row, index) => $toast.open(`Expanded ${row.user.first_name}`)"
             :show-detail-icon="showDetailIcon"
-        >
+            aria-next-label="Next page"
+            aria-previous-label="Previous page"
+            aria-page-label="Page"
+            aria-current-label="Current page">
 
             <template slot-scope="props">
                 <b-table-column field="id" label="ID" width="40" numeric>
