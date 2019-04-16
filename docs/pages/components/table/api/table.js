ac0046c85d1395c93adc85de07c4e111e1748af2
shelru <shelc@alumni.nottingham.ac.uk>
2019-04-16 11:00:18 +0400

    Doc's fix

--- docs/pages/components/table/api/table.js
+++ docs/pages/components/table/api/table.js
@@ -420,7 +420,7 @@ export default [
             {
                 name: '<code>width</code>',
                 description: 'Column fixed width in pixels',
-                type: 'Number',
+                type: 'String',
                 values: '—',
                 default: '—'
             },
