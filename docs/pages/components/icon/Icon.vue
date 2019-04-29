5db288c83b6667b6aa26cbf7c98b5be85295b86d
Albert Brand <albert.brand@gmail.com>
2019-04-29 17:28:37 +0200

    Fix typos in Icon code example

--- docs/pages/components/icon/Icon.vue
+++ docs/pages/components/icon/Icon.vue
@@ -59,19 +59,25 @@
                 ExFaCode,
                 ExObjectSyntaxCode,
                 usage: `
-                import { library } from "@fortawesome/fontawesome-svg-core";
+                import { library } from '@fortawesome/fontawesome-svg-core';
                 // internal icons
-                import { faCheck, faInfoCircle, faExclamationTriangle, faExclamationCirle,
+                import { faCheck, faInfoCircle, faExclamationTriangle, faExclamationCircle,
                     faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
-                    faEye,p faEyeSlash, faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+                    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload } from "@fortawesome/free-solid-svg-icons";
                 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-                library.add(faCheck);
-                ....
-                Vue.component("vue-fontawesome", FontAwesomeIcon);
+                library.add(faCheck, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+                    faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+                    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload);
+                Vue.component('vue-fontawesome', FontAwesomeIcon);
+
+                // ...
 
                 import Buefy from 'buefy'
-                Vue.use(Buefy, {defaultIconComponent: "vue-fontawesome"});`
+                Vue.use(Buefy, {
+                  defaultIconComponent: 'vue-fontawesome',
+                  defaultIconPack: 'fas',
+                });`
             }
         }
     }
