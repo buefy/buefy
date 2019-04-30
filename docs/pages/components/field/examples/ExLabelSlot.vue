d79138db23c780fedbd4ef4c05c4cc8abc7cd2d6
Yuxing Liao <yuxingliao@gmail.com>
2019-04-30 12:21:54 -0500

    Add example and api doc

---
+++ docs/pages/components/field/examples/ExLabelSlot.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <b-field
+            custom-class="is-medium"
+            horizontal>
+            <template slot="label">
+               With tooltip
+                <b-tooltip type="is-dark" label="Help text here for explanation">
+                    <b-icon size="is-small" icon="help-circle-outline"></b-icon>
+                </b-tooltip>
+            </template>
+            <b-input size="is-medium"></b-input>
+        </b-field>
+
+        <b-field>
+            <template slot="label">
+                Label with custom <span class="has-text-primary is-italic">style</span>
+            </template>
+            <b-input></b-input>
+        </b-field>
+    </section>
+</template>
