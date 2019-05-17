904decb74275bf3f2153f0fed061714835a8df5f
Filippo Fortino <filippofortino@gmail.com>
2019-05-17 18:39:44 +0200

    Added FontAwesome check-circle icon
    FontAwesome check-circle icon is also used, like in the notification component.

--- docs/pages/components/icon/Icon.vue
+++ docs/pages/components/icon/Icon.vue
@@ -61,12 +61,12 @@
                 usage: `
                 import { library } from '@fortawesome/fontawesome-svg-core';
                 // internal icons
-                import { faCheck, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+                import { faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
                     faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
                     faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload } from "@fortawesome/free-solid-svg-icons";
                 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-                library.add(faCheck, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+                library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
                     faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
                     faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload);
                 Vue.component('vue-fontawesome', FontAwesomeIcon);
