3099d46ef2c70ca450e86d51a216ca3d688d6921
Walter Tommasi <tommsi20@gmail.com>
2019-05-20 23:32:17 +0200

    Rever simple example datepicker

--- docs/pages/components/datepicker/examples/ExSimple.vue
+++ docs/pages/components/datepicker/examples/ExSimple.vue
@@ -1,19 +1,8 @@
 <template>
     <b-field label="Select a date">
-        <b-datepicker v-model="time"
+        <b-datepicker
             placeholder="Click to select..."
             icon="calendar-today">
         </b-datepicker>
-        {{!time ? 'nd' : time.toString()}}
     </b-field>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            time: null
-        }
-    }
-}
-</script>
