a9145434076649eb2cb443e6952d895156184ea3
Walter Tommasi <tommsi20@gmail.com>
2019-05-26 18:35:22 +0200

    Fix datepicker toggle unit test

--- src/components/datepicker/Datepicker.spec.js
+++ src/components/datepicker/Datepicker.spec.js
@@ -46,9 +46,10 @@ describe('BDatepicker', () => {
         expect($dropdown.isVisible()).toBeTruthy()
     })
 
-    it('call toggle method', () => {
+    it('call toggle method', async () => {
         wrapper.vm.$refs.dropdown.isActive = false
         wrapper.vm.toggle()
+        await wrapper.vm.$nextTick()
         expect(wrapper.vm.$refs.dropdown.isActive).toBeTruthy()
         expect($dropdown.isVisible()).toBeTruthy()
 
