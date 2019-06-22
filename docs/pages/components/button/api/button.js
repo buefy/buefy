d158d0e63b63715fab0dde405bea396380086b5d
Walter Tommasi <tommsi20@gmail.com>
2019-06-22 15:18:10 +0200

    Update button api docs

--- docs/pages/components/button/api/button.js
+++ docs/pages/components/button/api/button.js
@@ -71,7 +71,7 @@ export default [
                 name: '<code>tag</code>',
                 description: 'Button tag name',
                 type: 'String',
-                values: '<code>button</code>, <code>a</code>, <code>input</code>, <code>router-link</code>',
+                values: '<code>button</code>, <code>a</code>, <code>input</code>, <code>router-link</code>, <code>nuxt-link</code> or other nuxt alias',
                 default: '<code>button</code>'
             },
             {
