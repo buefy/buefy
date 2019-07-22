acc9d5905c4a8fa3da80f354ffd525108982dc84
JNieto <jorge.bbz@gmail.com>
2019-07-22 02:28:06 +0100

    Creates the navbar component

---
+++ src/components/navbar/NavbarItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <component
+        :is="tag"
+        class="navbar-item"
+        v-bind="$attrs"
+    >
+        <slot/>
+    </component>
+</template>
+
+<script>
+export default {
+    name: 'BNavbarItem',
+    props: {
+        tag: {
+            type: String,
+            default: 'a'
+        }
+    }
+}
+</script>
