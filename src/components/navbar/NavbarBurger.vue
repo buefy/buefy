259a940c42a0e92238aa6cc82cdad149a2b84c77
JNieto <jorge.bbz@gmail.com>
2019-07-22 02:28:33 +0100

    Creates the burger component

---
+++ src/components/navbar/NavbarBurger.vue
@@ -0,0 +1,25 @@
+<template>
+    <a
+        role="button"
+        class="navbar-burger burger"
+        :class="{ 'is-active': isOpened }"
+        aria-label="menu"
+        :aria-expanded="isOpened"
+    >
+        <span aria-hidden="true"/>
+        <span aria-hidden="true"/>
+        <span aria-hidden="true"/>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'NavbarBurger',
+    props: {
+        isOpened: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
