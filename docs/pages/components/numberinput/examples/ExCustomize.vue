215c06ccfeccc17c75dcfd20b96ad734523154bb
Alexandre Paradis <service.paradis@gmail.com>
2019-07-26 15:14:17 -0400

    Fixes #1576
    Manage label position for number input component
    Also works with sizes

--- docs/pages/components/numberinput/examples/ExCustomize.vue
+++ docs/pages/components/numberinput/examples/ExCustomize.vue
@@ -16,5 +16,18 @@
                 controls-rounded>
             </b-numberinput>
         </b-field>
+
+        <b-field label="Label inside" label-position="inside">
+            <b-numberinput></b-numberinput>
+        </b-field>
+        <b-field label="Label on border" label-position="on-border">
+            <b-numberinput></b-numberinput>
+        </b-field>
+        <b-field label="Compact with Label inside" label-position="inside">
+            <b-numberinput controls-position="compact"></b-numberinput>
+        </b-field>
+        <b-field label="Compact with Label on border" label-position="on-border">
+            <b-numberinput controls-position="compact"></b-numberinput>
+        </b-field>
     </section>
 </template>
