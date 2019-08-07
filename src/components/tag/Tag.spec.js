a3a04682d005fb671c3e3bc44a94070693a21f24
Alexandre Paradis <service.paradis@gmail.com>
2019-08-07 11:08:48 -0400

    Improve Tag and TagList unit tests
    - Also adds Snapshot validation

--- src/components/tag/Tag.spec.js
+++ src/components/tag/Tag.spec.js
@@ -1,11 +1,28 @@
 import { shallowMount } from '@vue/test-utils'
 import BTag from '@components/tag/Tag'
 
-describe('BTag', () => {
-    it('is called', () => {
-        const wrapper = shallowMount(BTag)
+let wrapper
 
+describe('BTag', () => {
+    beforeEach(() => {
+        wrapper = shallowMount(BTag)
+    })
+
+    it('is called', () => {
         expect(wrapper.name()).toBe('BTag')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('render correctly', () => {
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('emit close event when closing if not disabled', () => {
+        wrapper.setProps({ disabled: false })
+
+        const close = jest.fn()
+        wrapper.vm.$on('close', close)
+        wrapper.vm.close()
+        expect(close).toHaveBeenCalledTimes(1)
     })
 })
