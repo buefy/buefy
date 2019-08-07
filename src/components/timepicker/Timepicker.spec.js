d5b704f32602bb36bb4ad06b88256de33add6897
Alexandre Paradis <service.paradis@gmail.com>
2019-08-07 16:11:05 -0400

    Improve unit tests (#1640)
    * Improve Checkbox and CheckboxButton unit tests
    
    - Also adds snapshot validation
    
    * Improve Dialog unit tests
    
    * Improve Icon unit tests
    
    * Improve Radio and RadioButton unit tests
    
    - Also adds snapshot validation
    
    * Improve Snackbar unit tests
    
    - Also adds snapshot validation
    
    * Improve Switch unit tests
    
    * Improve Tag and TagList unit tests
    
    - Also adds Snapshot validation
    
    * Improve TimePicker unit tests
    
    * Improve TableMobileSort unit tests
    
    * Improve helpers unit tests

--- src/components/timepicker/Timepicker.spec.js
+++ src/components/timepicker/Timepicker.spec.js
@@ -8,4 +8,14 @@ describe('BTimepicker', () => {
         expect(wrapper.name()).toBe('BTimepicker')
         expect(wrapper.isVueInstance()).toBeTruthy()
     })
+
+    it('returns "1" for nativeStep is enableSeconds is true', () => {
+        const wrapper = shallowMount(BTimepicker, {
+            propsData: {
+                enableSeconds: true
+            }
+        })
+
+        expect(wrapper.vm.nativeStep).toBe('1')
+    })
 })
