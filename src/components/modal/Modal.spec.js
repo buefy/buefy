22cdf56b32ef07c1f921fc1f66426a5afb18ed8b
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 11:24:29 -0400

    Improve Modal unit tests and
    - Also add snapshot validation

--- src/components/modal/Modal.spec.js
+++ src/components/modal/Modal.spec.js
@@ -1,10 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
 import BModal from '@components/modal/Modal'
 
+let wrapper
+
 describe('BModal', () => {
+    beforeEach(() => {
+        wrapper = shallowMount(BModal, {
+            propsData: {
+                active: true
+            },
+            stubs: {
+                transition: false
+            }
+        })
+    })
+
     it('is called', () => {
-        const wrapper = shallowMount(BModal)
         expect(wrapper.name()).toBe('BModal')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('render correctly', () => {
+        expect(wrapper.html()).toMatchSnapshot()
     })
 })
