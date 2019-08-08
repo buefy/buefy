16677751588e59d63c8c3cc39c45c0277bdff245
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 11:28:44 -0400

    Improve Toast unit tests
    - Also add snapshot validation

--- src/components/toast/Toast.spec.js
+++ src/components/toast/Toast.spec.js
@@ -1,12 +1,20 @@
 import { shallowMount } from '@vue/test-utils'
 import BToast from '@components/toast/Toast'
 
+let wrapper
+
 describe('BToast', () => {
     HTMLElement.prototype.insertAdjacentElement = jest.fn()
+    beforeEach(() => {
+        wrapper = shallowMount(BToast)
+    })
 
     it('is called', () => {
-        const wrapper = shallowMount(BToast)
         expect(wrapper.name()).toBe('BToast')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('render correctly', () => {
+        expect(wrapper.html()).toMatchSnapshot()
     })
 })
