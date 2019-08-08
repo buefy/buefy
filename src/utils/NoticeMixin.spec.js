e06c5ec74541c718937e2826394e0535ad927b18
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 14:02:16 -0400

    Improve NoticeMixin

--- src/utils/NoticeMixin.spec.js
+++ src/utils/NoticeMixin.spec.js
@@ -18,4 +18,34 @@ describe('NoticeMixin', () => {
     it('is active', () => {
         expect(wrapper.vm.isActive).toBeTruthy()
     })
+
+    it('returns correct transition depending on position', () => {
+        const topTransition = {
+            enter: 'fadeInDown',
+            leave: 'fadeOut'
+        }
+        const bottomTransition = {
+            enter: 'fadeInUp',
+            leave: 'fadeOut'
+        }
+        const expected = {
+            'is-top': topTransition,
+            'is-top-right': topTransition,
+            'is-top-left': topTransition,
+            'is-bottom': bottomTransition,
+            'is-bottom-right': bottomTransition,
+            'is-bottom-left': bottomTransition
+        }
+        for (let [key, value] of Object.entries(expected)) {
+            wrapper.setProps({position: key})
+            expect(wrapper.vm.transition).toEqual(value)
+        }
+
+        expect(wrapper.vm.isActive).toBeTruthy()
+    })
+
+    it('reset isActive on close', () => {
+        wrapper.vm.close()
+        expect(wrapper.vm.isActive).toBeFalsy()
+    })
 })
