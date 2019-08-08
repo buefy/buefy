87565f9c1e8367d58bb2025ee840db3eed8060a0
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 11:25:51 -0400

    Improve Pagination unit tests
    - Also add snapshot validation

--- src/components/pagination/Pagination.spec.js
+++ src/components/pagination/Pagination.spec.js
@@ -1,10 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 import BPagination from '@components/pagination/Pagination'
 
+let wrapper
+
 describe('BPagination', () => {
+    beforeEach(() => {
+        wrapper = shallowMount(BPagination)
+    })
+
     it('is called', () => {
-        const wrapper = shallowMount(BPagination)
         expect(wrapper.name()).toBe('BPagination')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('render correctly', () => {
+        expect(wrapper.html()).toMatchSnapshot()
     })
 })
