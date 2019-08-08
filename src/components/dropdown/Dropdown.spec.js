da5bfbbb023bc602d81f693fcf2875242cf771ca
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 11:22:29 -0400

    Improve Dropdown unit tests and
    - Also add snapshot validation

--- src/components/dropdown/Dropdown.spec.js
+++ src/components/dropdown/Dropdown.spec.js
@@ -1,10 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 import BDropdown from '@components/dropdown/Dropdown'
 
+let wrapper
+
 describe('BDropdown', () => {
+    beforeEach(() => {
+        wrapper = shallowMount(BDropdown)
+    })
+
     it('is called', () => {
-        const wrapper = shallowMount(BDropdown)
         expect(wrapper.name()).toBe('BDropdown')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('render correctly', () => {
+        expect(wrapper.html()).toMatchSnapshot()
     })
 })
