1bac66ea644e3c1dd646920b359734c46094bba7
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 14:54:35 -0400

    Fix Dialog error

--- src/components/dialog/Dialog.spec.js
+++ src/components/dialog/Dialog.spec.js
@@ -12,31 +12,6 @@ describe('BDialog', () => {
         expect(wrapper.isVueInstance()).toBeTruthy()
     })
 
-    it('should return correct icon depending on type', () => {
-        const wrapper = shallowMount(BDialog, {
-            attachToDocument: true
-        })
-        const expected = {
-            'is-info': 'information',
-            'is-success': 'check-circle',
-            'is-warning': 'alert',
-            'is-danger': 'alert-circle',
-            'other': null
-        }
-        for (let [key, value] of Object.entries(expected)) {
-            wrapper.setProps({type: key})
-            expect(wrapper.vm.iconByType).toEqual(value)
-        }
-    })
-
-    it('should reset isActive on close', () => {
-        const wrapper = shallowMount(BDialog, {
-            attachToDocument: true
-        })
-        wrapper.vm.close()
-        expect(wrapper.vm.isActive).toBeFalsy()
-    })
-
     it('gives focus to the input element if it contains one', (done) => {
         const wrapper = shallowMount(BDialog, {
             attachToDocument: true,
