d5b638f712c63ba56a629c77f15896c326b2a813
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 16:44:20 -0400

    Improve Modal Unit tests

--- src/components/modal/Modal.spec.js
+++ src/components/modal/Modal.spec.js
@@ -23,4 +23,15 @@ describe('BModal', () => {
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
+
+    it('sets isActive when active prop is changed', () => {
+        wrapper.setProps({active: false})
+        expect(wrapper.vm.isActive).toBeFalsy()
+    })
+
+    it('emit events on close', () => {
+        wrapper.vm.close()
+        expect(wrapper.emitted()['close']).toBeTruthy()
+        expect(wrapper.emitted()['update:active']).toBeTruthy()
+    })
 })
