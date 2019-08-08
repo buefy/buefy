f91a008de4f5d544dd2ee3e14fc9ec094d92d9ce
Alexandre Paradis <service.paradis@gmail.com>
2019-08-08 11:26:44 -0400

    Improve Step unit tests
    - Also add snapshot validation

--- src/components/steps/StepItem.spec.js
+++ src/components/steps/StepItem.spec.js
@@ -1,15 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 import BSteps from '@components/steps/Steps'
+import BStepItem from '@components/steps/StepItem'
 
 let wrapper
 
 describe('BSteps', () => {
     beforeEach(() => {
-        wrapper = shallowMount(BSteps)
+        wrapper = shallowMount(BStepItem, {
+            parentComponent: BSteps
+        })
     })
 
     it('is called', () => {
-        expect(wrapper.name()).toBe('BSteps')
+        expect(wrapper.name()).toBe('BStepItem')
         expect(wrapper.isVueInstance()).toBeTruthy()
     })
 
