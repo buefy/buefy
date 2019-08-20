6078730ff10690f1738c5b1345cfa52bb8202125
Alexandre Paradis <service.paradis@gmail.com>
2019-08-20 10:22:46 -0400

    Fix icons in example

--- docs/pages/components/icon/Icon.vue
+++ docs/pages/components/icon/Icon.vue
@@ -114,8 +114,8 @@
                                 'chevron-down': 'arrow-down',
                                 eye: 'eye',
                                 'eye-off': 'unlock',
-                                'menu-down': 'menu-down',
-                                'menu-up': 'menu-up'
+                                'menu-down': 'arrow-down-circled',
+                                'menu-up': 'arrow-up-circled'
                             }
                         }
                     }
