4c1244af77c4cd5114e5886bd486389777b36bc0
Apolokak Lab <30395693+apolokaklab@users.noreply.github.com>
2019-08-27 00:36:02 +0700

    Update rate.js

--- docs/pages/components/rate/api/rate.js
+++ docs/pages/components/rate/api/rate.js
@@ -21,46 +21,60 @@ export default [
                 description: 'to specify the icon',
                 type: 'String',
                 values: '—',
-                default: '<code>fas fa-star</code>'
+                default: '<code>star</code>'
+            },
+            {
+                name: '<code>icon-pack</code>',
+                description: 'Icon pack to use',
+                type: 'String',
+                values: '<code>mdi</code>, <code>fa</code>, <code>fas</code>, <code>far</code>, <code>fad</code>, <code>fal</code>',
+                default: '<code>mdi</code>'
             },
             {
                 name: '<code>size</code>',
-                description: 'Size of the rate include <code>show-text</code>, <code>show-score</code> and <code>custom-text</code>, optional',
+                description: 'Size for rate include <code>show-text</code>, <code>show-score</code> and <code>custom-text</code>, optional',
                 type: 'String',
                 values: '<code>is-small</code>, <code>is-medium</code>, <code>is-large</code>',
                 default: '—'
             },
             {
+                name: '<code>spaced</code>',
+                description: 'Add spacing for icons',
+                type: 'Boolean',
+                values: '—',
+                default: '<code>False</code>'
+            },
+            {
                 name: '<code>disabled</code>',
-                description: 'Read-only Rate is for displaying rating score. Half star is supported',
+                description: 'Read only is for displaying rating score. Support decimal value for star.',
                 type: 'Boolean',
                 values: '—',
                 default: '<code>False</code>'
             },
             {
                 name: '<code>show-score</code>',
-                description: 'Display value, when disabled this default true.',
+                description: 'Display value.',
                 type: 'Boolean',
                 values: '—',
                 default: '<code>False</code>'
             },
             {
                 name: '<code>show-text</code>',
-                description: 'Display texts template. <code>show-score</code> and <code>show-text</code> cannot be true at the same time',
+                description: 'Display texts template. <code>show-score</code> and <code>show-text</code> cannot be true at the same time.',
                 type: 'Boolean',
                 values: '—',
                 default: '<code>False</code>'
             },
             {
                 name: '<code>custom-text</code>',
-                description: 'Display custom text only for <code>show-score</code>, like total Points or total user rating.',
+                description: 'Display custom text and only for <code>show-score</code>, like total points or total review.',
                 type: 'String',
                 values: '—',
-                default: '<code>—</code>'
+                default: '—'
             },
             {
                 name: '<code>texts</code>',
-                description: 'Texts template for <code>show-text</code>',
+                description: 'Texts template for <code>show-text</code>.',
                 type: 'Array',
                 values: '—',
                 default: '<code>Very Bad</code>, <code>Bad</code>, <code>Good</code>, <code>Very Good</code>, <code>Awesome</code>'
