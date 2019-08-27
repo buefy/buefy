b2bec3c6da4c7c1b3f0fe8e20e3215b0f588efd9
Alexandre Paradis <service.paradis@gmail.com>
2019-08-27 14:12:30 -0400

    Improve DatePicker Unit Tests

--- src/components/datepicker/DatepickerMonth.spec.js
+++ src/components/datepicker/DatepickerMonth.spec.js
@@ -5,6 +5,56 @@ import config, {setOptions} from '@utils/config'
 
 let wrapper
 
+const newDate = (y, m, d) => {
+    const date = new Date(Date.UTC(y, m, d))
+    date.getDate = jest.fn(() => date.getUTCDate())
+    return date
+}
+
+const thisMonth = new Date().getMonth()
+let events = [
+    newDate(2019, thisMonth, 2),
+    newDate(2019, thisMonth, 6),
+    {
+        date: newDate(2019, thisMonth, 6),
+        type: 'is-info'
+    },
+    {
+        date: newDate(2019, thisMonth, 8),
+        type: 'is-danger'
+    },
+    {
+        date: newDate(2019, thisMonth, 10),
+        type: 'is-success'
+    },
+    {
+        date: newDate(2019, thisMonth, 10),
+        type: 'is-link'
+    },
+    newDate(2019, thisMonth, 12),
+    {
+        date: newDate(2019, thisMonth, 12),
+        type: 'is-warning'
+    },
+    {
+        date: newDate(2019, thisMonth, 16),
+        type: 'is-danger'
+    },
+    newDate(2019, thisMonth, 20),
+    {
+        date: newDate(2019, thisMonth, 29),
+        type: 'is-success'
+    },
+    {
+        date: newDate(2019, thisMonth, 29),
+        type: 'is-warning'
+    },
+    {
+        date: newDate(2019, thisMonth, 29),
+        type: 'is-info'
+    }
+]
+
 describe('BDatepickerMonth', () => {
     beforeEach(() => {
         setOptions(Object.assign(config, {
@@ -12,7 +62,7 @@ describe('BDatepickerMonth', () => {
                 'January', 'February', 'March', 'April', 'May', 'June', 'July',
                 'August', 'September', 'October', 'November', 'December'
             ],
-            focusedDate: new Date('2018-07')
+            focusedDate: newDate(2018, 7, 1)
         }))
 
         wrapper = shallowMount(BDatepickerMonth, {
@@ -27,7 +77,7 @@ describe('BDatepickerMonth', () => {
         })
     })
 
-    const dateCreator = () => new Date()
+    const dateCreator = () => newDate()
 
     it('is called', () => {
         expect(wrapper.name()).toBe('BDatepickerMonth')
@@ -36,5 +86,19 @@ describe('BDatepickerMonth', () => {
 
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('manage events accordingly', () => {
+        wrapper.setProps({
+            events
+        })
+        expect(wrapper.vm.hasEvents).toBeTruthy()
+    })
+
+    it('emit chosen date', () => {
+        wrapper.vm.selectableDate = jest.fn(() => true)
+        wrapper.vm.emitChosenDate(5)
+        expect(wrapper.vm.selectableDate).toHaveBeenCalled()
+        expect(wrapper.emitted()['input']).toBeTruthy()
     })
 })
