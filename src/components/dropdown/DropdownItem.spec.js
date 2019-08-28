c584897b44175309621c4ff604e882b554c60283
Alexandre Paradis <service.paradis@gmail.com>
2019-08-27 22:09:25 -0400

    Improve Dropdown Unit tests

--- src/components/dropdown/DropdownItem.spec.js
+++ src/components/dropdown/DropdownItem.spec.js
@@ -2,11 +2,13 @@ import { shallowMount } from '@vue/test-utils'
 import BDropdownItem from '@components/dropdown/DropdownItem'
 
 let wrapper
+const dropdownSelected = 'val'
 const BDropdown = {
     template: '<b-dropdown-stub></b-dropdown-stub>',
     data() {
         return {
-            _isDropdown: true
+            _isDropdown: true,
+            selected: dropdownSelected
         }
     }
 }
@@ -25,5 +27,46 @@ describe('BDropdownItem', () => {
 
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('returns item classes accordingly', () => {
+        const value = dropdownSelected
+        const hasLink = true
+        const disabled = false
+        const paddingless = true
+        const isActive = true
+        wrapper.setProps({
+            value,
+            hasLink,
+            disabled,
+            paddingless
+        })
+        expect(wrapper.vm.itemClasses).toEqual({
+            'dropdown-item': !hasLink,
+            'is-disabled': disabled,
+            'is-paddingless': paddingless,
+            'is-active': isActive,
+            'has-link': hasLink
+        })
+    })
+
+    it('returns if item is clickable accordingly', () => {
+        expect(wrapper.vm.isClickable).toBeTruthy()
+
+        wrapper.setProps({ separator: true })
+        expect(wrapper.vm.isClickable).toBeFalsy()
+    })
+
+    it('react accordingly when the item is selected', () => {
+        wrapper.vm.$parent.selectItem = jest.fn()
+
+        wrapper.setProps({ separator: true })
+        wrapper.vm.selectItem()
+        expect(wrapper.vm.$parent.selectItem).not.toHaveBeenCalled()
+
+        wrapper.setProps({ separator: false })
+        wrapper.vm.selectItem()
+        expect(wrapper.vm.$parent.selectItem).toHaveBeenCalled()
+        expect(wrapper.emitted()['click']).toBeTruthy()
     })
 })
