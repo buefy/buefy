a3334ef6a62772d7271ed0bcd4d8b8b8adaeeb4d
Vlad Neacsu <me@vladneacsu.ro>
2019-09-04 13:24:21 +0300

    Add 'right' prop for NavbarDropdown (#1736)

--- docs/pages/components/navbar/api/navbar.js
+++ docs/pages/components/navbar/api/navbar.js
@@ -113,6 +113,13 @@ export default [
             type: 'Boolean',
             values: '-',
             default: 'false'
+        },
+        {
+            name: '<code>right</code>',
+            description: 'Dropdown will be anchored to the right side',
+            type: 'Boolean',
+            values: '-',
+            default: 'false'
         }
     ],
     slots: [
