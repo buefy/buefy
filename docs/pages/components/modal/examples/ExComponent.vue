d84e16d7ef8d2eab3885f6b3d859ef77a93c5b89
Alexandre Paradis <service.paradis@gmail.com>
2019-09-20 16:40:03 -0400

    Add an example in modal doc for trap-focus

--- docs/pages/components/modal/examples/ExComponent.vue
+++ docs/pages/components/modal/examples/ExComponent.vue
@@ -5,7 +5,7 @@
             Launch component modal
         </button>
 
-        <b-modal :active.sync="isComponentModalActive" has-modal-card>
+        <b-modal :active.sync="isComponentModalActive" has-modal-card trap-focus>
             <modal-form v-bind="formProps"></modal-form>
         </b-modal>
     </section>
