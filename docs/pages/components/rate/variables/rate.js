a77e917c28eee396f6104221844ddd936493ae19
Apolokak Lab <30395693+apolokaklab@users.noreply.github.com>
2019-09-23 13:06:10 +0700

    Add Rate component

---
+++ docs/pages/components/rate/variables/rate.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        name: '<code>$rate-color</code>',
+        default: '<code>$grey-lighter</code>'
+    },
+    {
+        name: '<code>$rate-color-active</code>',
+        default: '<code>darken($warning, 5%)</code>'
+    },
+    {
+      name: '<code>$rate-spaced</code>',
+      default: '<code>0.25rem</code>'
+    },
+    {
+      name: '<code>$rate-text-spaced</code>',
+      default: '<code>0.35rem</code>'
+    }
+]
