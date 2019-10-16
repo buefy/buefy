3c707c51b51021de02096c0dc12aaaad898c1a43
Alexandre Paradis <service.paradis@gmail.com>
2019-10-16 15:12:44 -0400

    Add a SCSS variable to control the breakpoint for mobile to dropdown (#1877)

---
+++ docs/pages/components/dropdown/variables/dropdown.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        name: '<code>$dropdown-mobile-breakpoint</code>',
+        description: 'The dropdown will be displayed as a modal below this value.',
+        default: '<code>$desktop</code>'
+    }
+]
