bf98d5609e1c6ebcc576ec30d33c172ced27c898
robert.marney <48888686+robertmarney@users.noreply.github.com>
2019-10-16 16:04:51 -0600

    Add Test

--- src/components/table/Table.spec.js
+++ src/components/table/Table.spec.js
@@ -1,10 +1,44 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import BTable from '@components/table/Table'
-
+let wrapper
 describe('BTable', () => {
+    beforeEach(() => {
+        wrapper = shallowMount(BTable)
+    })
+
     it('is called', () => {
-        const wrapper = mount(BTable)
         expect(wrapper.name()).toBe('BTable')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('has the filter row visible when searchable', () => {
+        wrapper.setProps({
+            searchable: 'true'
+        })
+        wrapper.setProps({
+            columns: [
+                {
+                    field: 'id',
+                    label: 'ID',
+                    width: '40',
+                    numeric: true
+                }
+            ]
+        })
+        // Don't show if no searchable column
+        expect(wrapper.vm.hasSearchablenewColumns).toBe(false)
+        // Show if one or more searchable column
+        wrapper.setProps({
+            columns: [
+                {
+                    field: 'id',
+                    label: 'ID',
+                    width: '40',
+                    numeric: true,
+                    searchable: true
+                }
+            ]
+        })
+        expect(wrapper.vm.hasSearchablenewColumns).toBe(true)
     })
 })
