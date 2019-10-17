bae12b88c1b7aca0a56e6b4c3a4f4ebb8bacb07e
bl <bl@moch.dk>
2019-10-17 17:45:55 +0200

    add simple test of table width

--- src/components/table/Table.spec.js
+++ src/components/table/Table.spec.js
@@ -8,12 +8,44 @@ describe('BTable', () => {
         wrapper = shallowMount(BTable)
     })
 
+    let tableCols = shallowMount(BTable, {
+        propsData: {
+            columns: [
+                {label: 'default', width: '100px'},
+                {label: 'pecent', width: '50%'},
+                {label: 'fixed_num', width: 100},
+                {label: 'fixed_str', width: '100'}
+            ]
+        }
+    })
+
     it('is called', () => {
         expect(wrapper.name()).toBe('BTable')
         expect(wrapper.isVueInstance()).toBeTruthy()
+
+        expect(tableCols.name()).toBe('BTable')
+        expect(tableCols.isVueInstance()).toBeTruthy()
     })
 
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('holds columns', () => {
+        let headers = tableCols.findAll('th')
+
+        expect(headers.length).toBeGreaterThanOrEqual(4)
+
+        let cols = headers.filter((th) => {
+            let div = th.find('div')
+
+            return div.classes('th-wrap')
+        })
+
+        expect(cols.length).toBe(4)
+        expect(cols.at(0).attributes('style')).toBe('width: 100px;')
+        expect(cols.at(1).attributes('style')).toBe('width: 50%;')
+        expect(cols.at(2).attributes('style')).toBe('width: 100px;')
+        expect(cols.at(3).attributes('style')).toBe('width: 100px;')
     })
 })
