60a3fffce08552c201037237f3d790c5a37f01d9
robert.marney <48888686+robertmarney@users.noreply.github.com>
2019-10-18 07:58:33 -0600

    Lint

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -114,13 +114,13 @@
                             :key="index"
                             :style="{
                                 width: column.width === undefined
-                            ? null
-                            : (isNaN(column.width) ? column.width : column.width + 'px') }">
+                                    ? null
+                                    : (isNaN(column.width) ? column.width : column.width + 'px') }">
                             <div class="th-wrap">
                                 <template v-if="column.searchable">
                                     <b-input
-                                    v-model="filters[column.field]"
-                                    :type="column.numeric ? 'number' : 'text'" />
+                                        v-model="filters[column.field]"
+                                        :type="column.numeric ? 'number' : 'text'" />
                                 </template>
                             </div>
                         </th>
