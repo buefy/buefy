503f955f37774753bd01e6fbd5b1fa648ba095ca
Adrien DAURIAT <16813527+dauriata@users.noreply.github.com>
2019-11-07 23:15:45 +0100

    Update TableColumn.vue
    Fix searchable column losing focus when cell removed

--- src/components/table/TableColumn.vue
+++ src/components/table/TableColumn.vue
@@ -64,6 +64,7 @@ export default {
     },
     beforeDestroy() {
         if( !this.$parent.visibleData.length ) return
+        if ( this.$parent.$children.filter(vm => vm.$options._componentTag === 'b-table-column' && vm.$data.newKey===this.newKey).length !== 1 ) return
         const index = this.$parent.newColumns.map(
             (column) => column.newKey).indexOf(this.newKey)
         if (index >= 0) {
