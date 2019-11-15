8dc48498b1801f6e0166ffd000901496b7047814
Adrien DAURIAT <16813527+dauriata@users.noreply.github.com>
2019-11-15 07:16:03 +0100

    Fix #1909 Searchable table Header disappear or loses focus (#1967)

--- src/components/table/TableColumn.vue
+++ src/components/table/TableColumn.vue
@@ -63,6 +63,8 @@ export default {
         this.addRefToTable()
     },
     beforeDestroy() {
+        if (!this.$parent.visibleData.length) return
+        if (this.$parent.$children.filter((vm) => vm.$options._componentTag === 'b-table-column' && vm.$data.newKey === this.newKey).length !== 1) return
         const index = this.$parent.newColumns.map(
             (column) => column.newKey).indexOf(this.newKey)
         if (index >= 0) {
