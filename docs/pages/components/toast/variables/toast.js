63e2b652b2ddb3f6d4f256c3dafef83b7d18abaf
Alexandre Paradis <service.paradis@gmail.com>
2019-12-10 12:05:18 -0500

    Add variables for custom Toast/Snackbar box shadow

--- docs/pages/components/toast/variables/toast.js
+++ docs/pages/components/toast/variables/toast.js
@@ -6,5 +6,9 @@ export default [
     {
         name: '<code>$toast-opacity</code>',
         default: '<code>0.92</code>'
-    }
+    },
+    {
+        name: '<code>$toast-box-shadow</code>',
+        default: '<code>0 1px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04)</code>'
+    },
 ]
