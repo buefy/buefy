8ad41ef943475ef045807295d5b5d94945ed0a7c
Alexandre Paradis <service.paradis@gmail.com>
2019-12-18 15:14:18 -0500

    Fix to keep defined slots position for tabs (#2092)

--- docs/pages/components/steps/Steps.vue
+++ docs/pages/components/steps/Steps.vue
@@ -2,6 +2,11 @@
     <div>
         <Example :component="ExSimple" :code="ExSimpleCode"/>
 
+        <Example :component="ExDynamic" :code="ExDynamicCode" title="Dynamic">
+            <p>Items can be created / modified / deleted and will always keep the defined order.</p>
+            <p>You can also use <code>v-if</code> to show (or not) a Step Item.</p>
+        </Example>
+
         <Example :component="ExIcons" :code="ExIconsCode" title="Icons"/>
 
         <Example :component="ExSizes" :code="ExSizesCode" title="Sizes"/>
@@ -20,6 +25,9 @@
     import ExSimple from './examples/ExSimple'
     import ExSimpleCode from '!!raw-loader!./examples/ExSimple'
 
+    import ExDynamic from './examples/ExDynamic'
+    import ExDynamicCode from '!!raw-loader!./examples/ExDynamic'
+
     import ExIcons from './examples/ExIcons'
     import ExIconsCode from '!!raw-loader!./examples/ExIcons'
 
@@ -35,10 +43,12 @@
                 api,
                 variables,
                 ExSimple,
+                ExDynamic,
                 ExIcons,
                 ExSizes,
                 ExTypes,
                 ExSimpleCode,
+                ExDynamicCode,
                 ExIconsCode,
                 ExSizesCode,
                 ExTypesCode
