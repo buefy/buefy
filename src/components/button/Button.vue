289582d9e37a354c97ee48fc718bdd1174d087cd
Maxim Neverov <nevmn@mail.ru>
2020-01-17 14:58:34 +0600

    Allow "tag" prop in PascalCase

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -89,6 +89,7 @@ export default {
                     'router-link',
                     'nuxt-link',
                     'n-link',
+                    'RouterLink',
                     'NuxtLink',
                     'NLink'
                 ].indexOf(value) >= 0
