1aced81d388c853f5ea61430da277a78b5d10173
Walter Tommasi <tommsi20@gmail.com>
2020-01-18 11:22:33 +0100

    Fix #2124 check null parentnode

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -108,7 +108,7 @@ export const isMobile = {
 export function removeElement(el) {
     if (typeof el.remove !== 'undefined') {
         el.remove()
-    } else if (typeof el.parentNode !== 'undefined') {
+    } else if (typeof el.parentNode !== 'undefined' && el.parentNode !== null) {
         el.parentNode.removeChild(el)
     }
 }
