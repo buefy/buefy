18ddeeedb07a242886bebff5c211bca367d627cd
Alexandre Paradis <service.paradis@gmail.com>
2020-02-20 17:55:03 -0500

    Pass isActive to dropdown trigger slot (#2246)

--- docs/pages/components/dropdown/examples/ExSimple.vue
+++ docs/pages/components/dropdown/examples/ExSimple.vue
@@ -1,9 +1,9 @@
 <template>
     <section>
         <b-dropdown aria-role="list">
-            <button class="button is-primary" slot="trigger">
+            <button class="button is-primary" slot="trigger" slot-scope="{ active }">
                 <span>Click me!</span>
-                <b-icon icon="menu-down"></b-icon>
+                <b-icon :icon="active ? 'menu-up' : 'menu-down'"></b-icon>
             </button>
 
             <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
