7e7ed248ff1d04e4e0e753e122ff3d7f4e80632e
Valar103769 <36926073+Valar103769@users.noreply.github.com>
2020-02-21 10:16:06 +0800

    Update Customization.vue

--- docs/pages/installation/Customization.vue
+++ docs/pages/installation/Customization.vue
@@ -61,7 +61,7 @@
             return {
                 sass: `
                 // Import Bulma's core
-                @import "~bulma/sass/utilities/_all";
+                @import "~bulma/sass/utilities/functions";
 
                 // Set your colors
                 $primary: #8c67ef;
@@ -70,16 +70,8 @@
                 $twitter-invert: findColorInvert($twitter);
 
                 // Setup $colors to use as bulma classes (e.g. 'is-twitter')
-                $colors: (
-                    "white": ($white, $black),
-                    "black": ($black, $white),
-                    "light": ($light, $light-invert),
-                    "dark": ($dark, $dark-invert),
+                $custom-colors: (
                     "primary": ($primary, $primary-invert),
-                    "info": ($info, $info-invert),
-                    "success": ($success, $success-invert),
-                    "warning": ($warning, $warning-invert),
-                    "danger": ($danger, $danger-invert),
                     "twitter": ($twitter, $twitter-invert)
                 );
 
