54a8b8f0538ab65c667a6b19332ff75b97f9f66f
Harrison Cutler <41189790+harrison9149@users.noreply.github.com>
2020-02-21 12:54:36 +0000

    Fixed spelling error in taginput api docs
    Line 142 The text was reading infinite-sroll I have updated this to match the event inifinite-scroll

--- docs/pages/components/taginput/api/taginput.js
+++ docs/pages/components/taginput/api/taginput.js
@@ -139,7 +139,7 @@ export default [
             },
             {
                 name: '<code>check-infinite-scroll</code>',
-                description: 'Makes the autocomplete component check if list reached scroll end and emit <code>infinite-sroll</code> event.',
+                description: 'Makes the autocomplete component check if list reached scroll end and emit <code>infinite-scroll</code> event.',
                 type: 'Boolean',
                 values: '—',
                 default: '<code>false</code>'
