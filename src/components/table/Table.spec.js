4cd31b1613358fe6cbd07ea9a96e3d749a3c75c4
Victor Cazanave <VictorCazanave@users.noreply.github.com>
2020-05-15 13:48:35 +0800

    Keep table search filtering when data are updated (#2511)
    * Keep table search filtering when data are updated
    
    * Add unit tests for searchable table

--- src/components/table/Table.spec.js
+++ src/components/table/Table.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
+import BInput from '@components/input/Input'
 import BTable from '@components/table/Table'
 
 describe('BTable', () => {
@@ -10,10 +11,10 @@ describe('BTable', () => {
     let tableCols = shallowMount(BTable, {
         propsData: {
             columns: [
-                {label: 'default', width: '100px'},
-                {label: 'pecent', width: '50%'},
-                {label: 'fixed_num', width: 100},
-                {label: 'fixed_str', width: '100'}
+                { label: 'default', width: '100px' },
+                { label: 'pecent', width: '50%' },
+                { label: 'fixed_num', width: 100 },
+                { label: 'fixed_str', width: '100' }
             ]
         }
     })
@@ -74,5 +75,68 @@ describe('BTable', () => {
         expect(cols.at(1).attributes('style')).toBe('width: 50%;')
         expect(cols.at(2).attributes('style')).toBe('width: 100px;')
         expect(cols.at(3).attributes('style')).toBe('width: 100px;')
+    })
+
+    describe('Searchable', () => {
+        const data = [
+            { id: 1, name: 'Jesse' },
+            { id: 2, name: 'John' },
+            { id: 3, name: 'Tina' },
+            { id: 4, name: 'Anne' },
+            { id: 5, name: 'Clarence' }
+        ]
+        let headRows
+        let bodyRows
+        let searchInput
+
+        beforeEach(() => {
+            wrapper = shallowMount(BTable, {
+                propsData: {
+                    columns: [
+                        { label: 'ID', field: 'id', numeric: true },
+                        { label: 'Name', field: 'name', searchable: true }
+                    ],
+                    data
+                }
+            })
+            headRows = wrapper.findAll('thead tr')
+            bodyRows = wrapper.findAll('tbody tr')
+            searchInput = wrapper.find(BInput)
+        })
+
+        it('displays filter row when at least one column is searchable', () => {
+            expect(headRows).toHaveLength(2)
+        })
+
+        it('displays filter input only on searchable columns', () => {
+            const filterCells = headRows.at(1).findAll('.th-wrap')
+
+            expect(filterCells.at(0).isEmpty()).toBe(true) // ID column is not searchable
+            expect(filterCells.at(1).contains(BInput)).toBe(true) // Name column is searchable
+        })
+
+        it('displays all data', () => {
+            expect(bodyRows).toHaveLength(5)
+        })
+
+        it('displays filtered data when searching', () => {
+            searchInput.vm.$emit('input', 'J')
+            bodyRows = wrapper.findAll('tbody tr')
+
+            expect(bodyRows).toHaveLength(2) // Jesse and John
+        })
+
+        it('displays filtered data when searching and updating data', () => {
+            searchInput.vm.$emit('input', 'J')
+            wrapper.setProps({
+                data: [
+                    ...data,
+                    { id: 6, name: 'Justin' }
+                ]
+            })
+            bodyRows = wrapper.findAll('tbody tr')
+
+            expect(bodyRows).toHaveLength(3) // Jesse, John and Justin
+        })
     })
 })
