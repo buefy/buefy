8fb0bb1bd17c82541f07be42c3c899e871b31b06
Adrien Foulon <adrien.foulon@tukan.hu>
2020-06-23 16:48:41 +0200

    Add exponential prop to numberinput

--- src/components/numberinput/Numberinput.spec.js
+++ src/components/numberinput/Numberinput.spec.js
@@ -3,6 +3,10 @@ import BNumberinput from '@components/numberinput/Numberinput'
 
 let wrapper
 
+const wait = (time) => {
+    return new Promise((resolve) => setTimeout(resolve, time))
+}
+
 describe('BNumberinput', () => {
     describe('Rendered', () => {
         beforeEach(() => {
@@ -33,6 +37,64 @@ describe('BNumberinput', () => {
         it('default value is 0', () => {
             const input = wrapper.find('input')
             expect(input.element.value).toBe('0')
+        })
+
+        it('increments/decrements on long pressing exponentially', async () => {
+            wrapper.setProps({exponential: true, value: 1, step: 1})
+
+            wrapper.find('.control.plus button').trigger('mousedown')
+
+            await wait(1500)
+
+            wrapper.find('.control.plus').trigger('mouseup')
+            await wrapper.vm.$nextTick()
+
+            expect(wrapper.vm.computedValue).toBeGreaterThan(90)
+            expect(wrapper.vm.computedValue).toBeLessThan(150)
+
+            wrapper.setProps({exponential: 3, value: 0, step: 1})
+
+            wrapper.find('.control.plus button').trigger('mousedown')
+
+            await wait(1000)
+
+            wrapper.find('.control.plus').trigger('mouseup')
+
+            expect(wrapper.vm.computedValue).toBeGreaterThan(200)
+            expect(wrapper.vm.computedValue).toBeLessThan(300)
+        })
+
+        it('increments/decrements on long pressing', async () => {
+            jest.useFakeTimers()
+
+            let val = 0
+
+            // Increment
+            wrapper.find('.control.plus button').trigger('mousedown')
+            val++
+
+            // await wait(2000)
+            jest.runOnlyPendingTimers()
+            jest.runOnlyPendingTimers()
+            jest.runOnlyPendingTimers()
+            val += 3
+
+            wrapper.find('.control.plus').trigger('mouseup')
+            expect(wrapper.vm.computedValue).toBe(val)
+
+            // Decrement
+            wrapper.find('.control.minus button').trigger('mousedown')
+            val--
+
+            jest.runOnlyPendingTimers()
+            jest.runOnlyPendingTimers()
+            val -= 2
+
+            wrapper.find('.control.minus button').trigger('mouseup')
+            // Trigger it another time to check for unexpected browser behavior
+            wrapper.find('.control.minus button').trigger('mouseup')
+
+            expect(wrapper.vm.computedValue).toBe(val)
         })
     })
 
@@ -101,6 +163,22 @@ describe('BNumberinput', () => {
             wrapper.setProps({ min })
             wrapper.vm.computedValue = ''
             expect(wrapper.vm.computedValue).toBe(min)
+        })
+
+        it('increments/decrements on click', async () => {
+            wrapper.setProps({value: 5, step: 1})
+
+            wrapper.find('.control.plus button').trigger('click')
+
+            await wrapper.vm.$nextTick()
+
+            expect(wrapper.vm.computedValue).toBe(6)
+
+            wrapper.find('.control.minus button').trigger('click')
+
+            await wrapper.vm.$nextTick()
+
+            expect(wrapper.vm.computedValue).toBe(5)
         })
     })
 })
