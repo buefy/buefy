4c03fa8c08e54371b74d8717c25d200585f61c8c
Alexandre Paradis <service.paradis@gmail.com>
2020-07-07 08:39:20 -0400

    Improve Carousel Unit tests

--- src/components/carousel/CarouselItem.spec.js
+++ src/components/carousel/CarouselItem.spec.js
@@ -1,26 +1,57 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import BCarousel from '@components/carousel/Carousel'
 import BCarouselItem from '@components/carousel/CarouselItem'
-import { default as ProviderParentMixin, Sorted } from '../../utils/ProviderParentMixin'
 
 let wrapper
-const BCarousel = {
-    template: '<b-carousel-stub></b-carousel-stub>',
-    mixins: [ProviderParentMixin('carousel', Sorted)]
+let wrapperParent
+let wrapperCarousel
+
+const WrapperComp = {
+    data() {
+        return {
+            show1: true
+        }
+    },
+    template: `
+        <BCarousel ref="carousel">
+            <BCarouselItem value="item1"/>
+            <BCarouselItem ref="testItem" value="item2"/>
+            <BCarouselItem value="item3"/>
+        </BCarousel>`,
+    components: {
+        BCarousel, BCarouselItem
+    }
 }
 
 describe('BCarouselItem', () => {
     beforeEach(() => {
-        wrapper = shallowMount(BCarouselItem, {
-            parentComponent: BCarousel
-        })
+        wrapperParent = mount(WrapperComp, { sync: false })
+        wrapperCarousel = wrapperParent.find({ ref: 'carousel' })
+        wrapper = wrapperParent.find({ ref: 'testItem' })
     })
 
-    it('is vue instance', () => {
+    it('is called', () => {
         expect(wrapper.name()).toBe('BCarouselItem')
         expect(wrapper.isVueInstance()).toBeTruthy()
     })
 
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('computes its position correctly', () => {
+        expect(wrapper.vm.index).toBe(1)
+    })
+
+    it('transition correctly', () => {
+        wrapperCarousel.vm.changeActive(2)
+        expect(wrapper.vm.transition).toBe('slide-prev')
+
+        wrapperCarousel.vm.changeActive(0)
+        expect(wrapper.vm.transition).toBe('slide-next')
+
+        wrapperCarousel.setProps({ animated: 'fade' })
+        wrapperCarousel.vm.changeActive(0)
+        expect(wrapper.vm.transition).toBe('fade')
     })
 })
