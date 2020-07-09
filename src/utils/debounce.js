8e4d354d4ccfacf156677f415f3f02576a45edb3
Sc0ra <axelfournier78@gmail.com>
2020-07-09 19:42:00 +0200

    feat: (utils) add debounce function

---
+++ src/utils/debounce.js
@@ -0,0 +1,15 @@
+export default function (func, wait, immediate) {
+    var timeout
+    return function () {
+        var context = this
+        var args = arguments
+        var later = function () {
+            timeout = null
+            if (!immediate) func.apply(context, args)
+        }
+        var callNow = immediate && !timeout
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+        if (callNow) func.apply(context, args)
+    }
+}
