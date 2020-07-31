fafa1ae89858e36bf9750b185c296d10906a2b58
apolokaklab <apolokak19@gmail.com>
2020-08-01 06:40:18 +0700

    Fixes auto open browser when run dev

--- build/dev-server.js
+++ build/dev-server.js
@@ -5,7 +5,7 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
-var opn = require('opn')
+var open = require('open')
 var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
@@ -76,7 +76,7 @@ devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
+    open(uri)
   }
   _resolve()
 })
