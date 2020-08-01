e4774bc71f271bcce20ecdd3d4c8cf74a172c33a
Apolokak Lab <30395693+apolokaklab@users.noreply.github.com>
2020-08-02 01:30:16 +0700

    Improved on the docs (#2722)
    * cleaning of warning issue on CLI when run dev and test
    
    * Fixes auto open browser when run dev
    
    * refactor: Use const/let instead of var with strict mode
    
    * Fix vetur script when API don't have props and update script build:lib
    
    * Add banner on cli when run dev and build docs
    
    * Improved on building the docs
    
    * update Use const/let instead of var

--- build/build.js
+++ build/build.js
@@ -1,22 +1,20 @@
+'use strict'
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
+const rm = require('rimraf')
+const chalk = require('chalk')
+const webpack = require('webpack')
+const config = require('../config')
+const utils = require('./utils')
+const webpackConfig = require('./webpack.prod.conf')
 
-var spinner = ora('building for production...')
-spinner.start()
+console.log(`    ${utils.bannerCLI()}`)
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+rm(config.build.assetsRoot, err => {
   if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
+  webpack(webpackConfig, (err, stats) => {
     if (err) throw err
     process.stdout.write(stats.toString({
       colors: true,
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,29 +1,30 @@
-var fs = require('fs')
-var path = require('path')
-var utils = require('./utils')
-var webpack = require('webpack')
-var config = require('../config')
-var merge = require('webpack-merge')
-var baseWebpackConfig = require('./webpack.base.conf')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var PrerenderSPAPlugin = require('prerender-spa-plugin')
-var Renderer = PrerenderSPAPlugin.PuppeteerRenderer
-var routes = require('../docs/data/routes.json')
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const routes = require('../docs/data/routes.json')
 
 // route paths
-var paths = []
+const paths = []
 for (let key in routes) {
     paths.push(routes[key].path)
 }
 
-var env = process.env.NODE_ENV === 'testing'
+const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : config.build.env
 
-var webpackConfig = merge(baseWebpackConfig, {
+const webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
@@ -42,11 +43,13 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      sourceMap: true
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        },
+        sourceMap: true
+      }
     }),
     // extract css into its own file
     new ExtractTextPlugin({
@@ -83,7 +86,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks: function (module, count) {
+      minChunks: function (module) {
         // any required modules inside node_modules are extracted to vendor
         return (
           module.resource &&
@@ -112,7 +115,7 @@ var webpackConfig = merge(baseWebpackConfig, {
 })
 
 if (config.build.productionGzip) {
-  var CompressionWebpackPlugin = require('compression-webpack-plugin')
+ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
@@ -130,7 +133,7 @@ if (config.build.productionGzip) {
 }
 
 if (config.build.bundleAnalyzerReport) {
-  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 }
 
@@ -138,8 +141,14 @@ if (process.env.NODE_ENV !== 'test') {
   webpackConfig.plugins.push(new PrerenderSPAPlugin({
     staticDir: config.build.assetsRoot,
     routes: paths,
+    postProcess: (renderedRoute) => {
+      renderedRoute.html = renderedRoute.html
+        .replace('id="app"', 'id="app" data-server-rendered="true"')
+
+      return renderedRoute
+    },
     renderer: new Renderer({
-      maxConcurrentRoutes: 6,
+      maxConcurrentRoutes: 5,
       renderAfterDocumentEvent: 'render-event',
       injectProperty: '__PRERENDER_INJECTED',
       inject: {
