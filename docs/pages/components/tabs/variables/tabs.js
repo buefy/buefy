909dcc491b8b4392d1371bd318b30d75d2fb8400
Alexandre Paradis <service.paradis@gmail.com>
2020-08-12 14:55:48 -0400

    Improve styles and add variables

---
+++ docs/pages/components/tabs/variables/tabs.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: '<code>$tabs-focused-outline</code>',
+        default: '<code>0</code>'
+    },
+    {
+        name: '<code>$tabs-link-focus-active-border-bottom-color</code>',
+        default: '<code>$tabs-link-active-border-bottom-color</code>'
+    },
+    {
+        name: '<code>$tabs-link-focus-border-bottom-color</code>',
+        default: '<code>$tabs-link-hover-border-bottom-color</code>'
+    }
+]
