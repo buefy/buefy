5f5f76411593955a0f9d12548d2419d55a63c8dd
Alexandre Paradis <service.paradis@gmail.com>
2020-08-19 09:55:58 -0400

    Improve b-image Placeholder Example
    - Add an example to demonstrate the lqip effect

--- docs/pages/components/image/examples/ExPlaceholder.vue
+++ docs/pages/components/image/examples/ExPlaceholder.vue
@@ -1,14 +1,20 @@
 <template>
     <section>
         <b-image
-            src="https://picsum.photos/800/450"
+            src="https://picsum.photos/id/1074/800/400"
             placeholder="/static/img/icons/favicon-32x32.png"
-            ratio="16by9"
+            ratio="2by1"
         ></b-image>
         <br>
         <b-image
-            src="https://picsum.photos/800/450"
-            ratio="16by9"
+            src="https://picsum.photos/id/237/800/400"
+            placeholder="https://picsum.photos/id/237/200/100?blur=10"
+            ratio="2by1"
+        ></b-image>
+        <br>
+        <b-image
+            src="https://picsum.photos/id/1062/800/400"
+            ratio="2by1"
         >
             <b-skeleton
                 class="skeleton-placeholder"
