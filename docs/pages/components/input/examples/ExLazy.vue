4e07eb7f3e02f52b76c848396dfd51457ec97be0
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-08-28 16:50:13 +0200

    Added Lazy to b-input
    Included docs and example

---
+++ docs/pages/components/input/examples/ExLazy.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <b-field>
+            <b-switch v-model="beLazy">Enable lazy</b-switch>
+        </b-field>
+        <b-field>
+            <b-input type="text"
+                :lazy="beLazy"
+                v-model="value"
+                placeholder="Input">
+            </b-input>
+        </b-field>
+        <p class="content">
+            Value is: {{value}}
+        </p>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            beLazy: false,
+            value: null
+        }
+    }
+}
+</script>
