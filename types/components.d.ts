bb824207ae0fedba65a3d4188615e7548f4a0f42
Charlie <bruno11.francisco@gmail.com>
2020-10-07 22:09:01 +0200

    Add table column typescript definitions (#2953)
    - Added table column typescript definitions
    
    Co-authored-by: Bruno Francisco <bruno.francisco@foreo.com>

--- types/components.d.ts
+++ types/components.d.ts
@@ -395,4 +395,21 @@ export declare const ConfigProgrammatic: {
     setOptions: (params: BuefyConfig) => any
 }
 
-
+export interface TableColumn {
+    label?: string;
+    'custom-key'?: string | number;
+    field?: string | number;
+    meta?: any;
+    width?: number | string;
+    numeric?: boolean;
+    centered?: boolean;
+    sortable?: boolean;
+    visible?: boolean;
+    'custom-sort'?: (a: object, b: object, isAsc: boolean) => void;
+    searchable?: boolean;
+    subheading?: string | number;
+    sticky?: boolean;
+    'header-selectable'?: boolean;
+    'header-class'?: string;
+    'cell-class'?: string;
+}
