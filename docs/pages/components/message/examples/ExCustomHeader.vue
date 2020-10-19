04f30d151d99809968f2d31f422984b7cb1d3af7
m4st3rm4tz3 <wilke.mat@gmx.de>
2020-10-19 21:43:06 +0200

    added custom header example

---
+++ docs/pages/components/message/examples/ExCustomHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+        <b-message>
+            <template #header>
+                Custom header with <b-tag>b-tag</b-tag>
+            </template>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+        </b-message>
+    </section>
+</template>
