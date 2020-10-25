15a12054d717a4f19ea4b36fbd02308edc16198d
Adam Greenhall <adam.greenhall@gmail.com>
2020-10-25 12:07:48 -0700

    add invalid event to upload (#3001)

--- src/components/upload/Upload.vue
+++ src/components/upload/Upload.vue
@@ -159,6 +159,9 @@ export default {
                     }
                 }
             }
+            if (!valid) {
+                this.$emit("invalid");
+            }
             return valid
         }
     }
