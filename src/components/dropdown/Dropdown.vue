581eed1f8daf6f17127fafa451fbb11f610dbebf
Victor Cazanave <VictorCazanave@users.noreply.github.com>
2020-11-18 20:33:11 +0800

    Remove array mutation on dropdown (#3047)

--- src/components/dropdown/Dropdown.vue
+++ src/components/dropdown/Dropdown.vue
@@ -187,19 +187,20 @@ export default {
     },
     methods: {
         /**
-        * Click listener from DropdownItem.
-        *   1. Set new selected item.
-        *   2. Emit input event to update the user v-model.
-        *   3. Close the dropdown.
-        */
+         * Click listener from DropdownItem.
+         *   1. Set new selected item.
+         *   2. Emit input event to update the user v-model.
+         *   3. Close the dropdown.
+         */
         selectItem(value) {
             if (this.multiple) {
                 if (this.selected) {
-                    const index = this.selected.indexOf(value)
-                    if (index === -1) {
-                        this.selected.push(value)
+                    if (this.selected.indexOf(value) === -1) {
+                        // Add value
+                        this.selected = [...this.selected, value]
                     } else {
-                        this.selected.splice(index, 1)
+                        // Remove value
+                        this.selected = this.selected.filter((val) => val !== value)
                     }
                 } else {
                     this.selected = [value]
