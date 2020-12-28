3590a055315512888d8273914349213ab0d22fbc
Karel Vanden Bussche <omg.kvb@gmail.com>
2020-12-28 17:30:03 +0100

    Depending on the isActive variable, the z-index of the parent element (_bodyEl) should be -1 or 0

--- src/components/dropdown/Dropdown.vue
+++ src/components/dropdown/Dropdown.vue
@@ -336,6 +336,7 @@ export default {
                     parentNode.classList.forEach((item) => {
                         parent.classList.add(item)
                     })
+                    parentNode.style.zIndex = this.isActive ? '-1' : '0'
                 }
                 const rect = trigger.getBoundingClientRect()
                 let top = rect.top + window.scrollY
