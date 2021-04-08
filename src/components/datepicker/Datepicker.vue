a935f4cb368e5ef4413fe5f3d7441c0edb66af3b
Marko Kos <marko.kos@infoart.hr>
2021-04-08 16:04:40 +0200

    Datepicker with multiple - stay on selected month (#3224)

--- src/components/datepicker/Datepicker.vue
+++ src/components/datepicker/Datepicker.vue
@@ -734,7 +734,7 @@ export default {
         },
         updateInternalState(value) {
             const currentDate = Array.isArray(value)
-                ? (!value.length ? this.dateCreator() : value[0])
+                ? (!value.length ? this.dateCreator() : value[value.length - 1])
                 : (!value ? this.dateCreator() : value)
             this.focusedDateData = {
                 day: currentDate.getDate(),
