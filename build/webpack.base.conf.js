300fb847f4bb597a2d5fe714bdfe18a2c7d7d69b
Kikuo Emoto <kikuomax@gmail.com>
2021-04-26 01:51:26 +0900

    chore: update alias to vue
    - The alias to 'vue' is replaced:
      `vue/dist/vue.esm.js` --> `vue/dist/vue.cjs.js`.
      Because `vue.esm.js` no longer exists in `vue/dist` directory.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -24,7 +24,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      'vue$': 'vue/dist/vue.cjs.js',
       '@': resolve('docs'),
       '@components': resolve('src/components'),
       '@utils': resolve('src/utils')
