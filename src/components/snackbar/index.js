43193759a7d88d2ba04407fb8829f0e9196ddf83
Kikuo Emoto <kikuomax@gmail.com>
2021-06-12 01:37:39 +0900

    chore(lib): update Snackbar
    - `Snackbar` migrates to Vue 3. Changes sufficient to render the
      documentation page of `Snackbar` are made. A commit to update the
      documentation page will follow this commit.
    - In `src/components/snackbar/index.js`,
        - `open` creates a brand-new Vue app that renders `Snackbar` by
          `createApp`, and mount it on a new `div` element. This is done
          because Vue 3 no longer provides `Vue.extend`, and neither allows
          directly newing a component. `open` does not return an instance
          of `Snackbar` but a wrapper that exposes the `close` method so
          that it can programmatically be closed. The app unmounts itself
          when the `Snackbar` is closed.
        - Adds the `$buefy` global property to the new Vue app so that
          `globalNoticeInterval` is available.

--- src/components/snackbar/index.js
+++ src/components/snackbar/index.js
@@ -1,14 +1,13 @@
+import { createApp, h as createElement } from 'vue'
+
 import Snackbar from './Snackbar.vue'
 
-import config, { VueInstance } from '../../utils/config'
+import config from '../../utils/config'
 import { merge } from '../../utils/helpers'
 import { use, registerComponentProgrammatic } from '../../utils/plugins'
 
-let localVueInstance
-
 const SnackbarProgrammatic = {
     open(params) {
-        let parent
         if (typeof params === 'string') {
             params = {
                 message: params
@@ -21,7 +20,6 @@ const SnackbarProgrammatic = {
             queue: true
         }
         if (params.parent) {
-            parent = params.parent
             delete params.parent
         }
         let slot
@@ -30,24 +28,51 @@ const SnackbarProgrammatic = {
             delete params.message
         }
         const propsData = merge(defaultParam, params)
-        const vm = typeof window !== 'undefined' && window.Vue ? window.Vue : localVueInstance || VueInstance
-        const SnackbarComponent = vm.extend(Snackbar)
-        const component = new SnackbarComponent({
-            parent,
-            el: document.createElement('div'),
-            propsData
+        const container = document.createElement('div')
+        const vueInstance = createApp({
+            data() {
+                return {
+                    snackbarVNode: null
+                }
+            },
+            methods: {
+                close() {
+                    const snackbar =
+                        this.snackbarVNode.component?.exposed ||
+                        this.snackbarVNode.component?.proxy
+                    snackbar?.close()
+                }
+            },
+            render() {
+                this.snackbarVNode = createElement(
+                    Snackbar,
+                    {
+                        ...propsData,
+                        onClose() {
+                            if (typeof propsData.onClose === 'function') {
+                                propsData.onClose()
+                            }
+                            // timeout for the animation complete
+                            // before unmounting
+                            setTimeout(() => {
+                                vueInstance.unmount()
+                            }, 150)
+                        }
+                    },
+                    slot != null ? { default: () => slot } : undefined
+                )
+                return this.snackbarVNode
+            }
         })
-        if (slot) {
-            component.$slots.default = slot
-            component.$forceUpdate()
-        }
-        return component
+        // adds $buefy global property so that
+        // this.$buefy.globalNoticeInterval is available
+        vueInstance.config.globalProperties.$buefy = {}
+        return vueInstance.mount(container)
     }
 }
 
 const Plugin = {
     install(Vue) {
-        localVueInstance = Vue
         registerComponentProgrammatic(Vue, 'snackbar', SnackbarProgrammatic)
     }
 }
