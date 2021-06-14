3a46b78fcd58275ef9625eb222a7d436c16da499
Kikuo Emoto <kikuomax@gmail.com>
2021-06-15 02:24:04 +0900

    fix(lib): fix makeUniqueId
    - The bug that `makeUniqueId` did not correctly convert a generated
      byte sequence into a hexiadecimal representation is fixed. The `map`
      function of `Array` instead of that of a `TypedArray` has to be
      used, because the `map` function of a `TypedArray` casts a mapped
      value to its element type (`Uint8` in this case) but the expected
      type here is a `string`.

--- src/utils/make-unique-id.js
+++ src/utils/make-unique-id.js
@@ -1,7 +1,5 @@
 export function makeUniqueId() {
     const values = new Uint8Array(12)
     window.crypto.getRandomValues(values)
-    return values
-        .map((v) => v.toString(16))
-        .join('')
+    return Array.prototype.map.call(values, (v) => v.toString(16)).join('')
 }
