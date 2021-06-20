1411177521dc120a3add01ec1e2cfaf8cda3eca9
Kikuo Emoto <kikuomax@gmail.com>
2021-06-21 02:50:08 +0900

    chore(lib): update Webpack prod config
    - The Webpack config for production is updated.
        - `UglifyJsPlugin` is moved from `plugins` to the `optimization`.
          `uglifyOptions` is updated.
        - `webpack.optimize.CommonsChunkPlugin`s are removed because the
          plugin is no longer provided since Webpack 4.
            - It seems that `SplitChunksPlugin` does the job of the
              `vendor` chunk by default.
            - I could not figure out how to handle the `manifest` chunk
              with `SplitChunksPlugin`.
        - The `chunkSortMode` option of `HtmlWebpackPlugin` seems
          unnecessary.
          https://github.com/jantimon/html-webpack-plugin/issues/1055
        - The argument of `CopyWebpackPlugin` is updated.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -39,18 +39,21 @@ const webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          sourceMap: true
+        }
+      })
+    ]
+  },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
-    }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        compress: {
-          warnings: false
-        },
-        sourceMap: true
-      }
     }),
     // extract css into its own file
     new MiniCssExtractPlugin({
@@ -80,38 +83,22 @@ const webpackConfig = merge(baseWebpackConfig, {
         removeAttributeQuotes: true
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
-    }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
       }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
+      // chunksSortMode: 'dependency' is no longer necessary.
+      // https://github.com/jantimon/html-webpack-plugin/issues/1055
     }),
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../static'),
+          to: config.build.assetsSubDirectory,
+          globOptions: {
+            ignore: ['.*']
+          }
+        }
+      ]
+    })
   ]
 })
 
