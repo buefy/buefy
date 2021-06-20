f6c5db82fecfe0ea0dcdd0249f16c2578144f96b
Kikuo Emoto <kikuomax@gmail.com>
2021-06-20 14:45:45 +0900

    chore(lib): lint src/utils/debounce
    - ESLint fix is applied to `src/utils/debounce.js`.

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,13 +1,13 @@
 export default function (func, wait, immediate) {
-    var timeout
+    let timeout
     return function () {
-        var context = this
-        var args = arguments
-        var later = function () {
+        const context = this
+        const args = arguments
+        const later = function () {
             timeout = null
             if (!immediate) func.apply(context, args)
         }
-        var callNow = immediate && !timeout
+        const callNow = immediate && !timeout
         clearTimeout(timeout)
         timeout = setTimeout(later, wait)
         if (callNow) func.apply(context, args)
