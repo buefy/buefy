ae4a1f5b5ed22f491bb7a33358c8294dbc36320d
adrien <16813527+dauriata@users.noreply.github.com>
2021-08-24 09:39:30 +0200

    autocomplete fix with keepFirst

--- src/components/autocomplete/Autocomplete.vue
+++ src/components/autocomplete/Autocomplete.vue
@@ -343,6 +343,8 @@ export default {
                 this.$nextTick(() => {
                     if (this.isActive) {
                         this.selectFirstOption(this.computedData)
+                    } else {
+                        this.setHovered(null)
                     }
                 })
             }
@@ -380,11 +382,14 @@ export default {
         /**
          * Select first option
          */
-        selectFirstOption(element) {
+        selectFirstOption(computedData) {
             this.$nextTick(() => {
-                if (element.length) {
+                const nonEmptyElements = computedData.filter(
+                    (element) => element.items && element.items.length
+                )
+                if (nonEmptyElements.length) {
                     // If has visible data or open on focus, keep updating the hovered
-                    const option = element[0].items[0]
+                    const option = nonEmptyElements[0].items[0]
                     if (this.openOnFocus || (this.newValue !== '' && this.hovered !== option)) {
                         this.setHovered(option)
                     }
