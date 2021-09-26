18386a04cdca1b372366d5c0e52474dde61924d9
Mouad Taoussi <50294614+mouadTaoussi@users.noreply.github.com>
2021-09-26 22:17:51 +0100

    [Feature Request]: Added progress-bar of remaining time in Notification Component (#3520)
    * Initial code
    
    * Finishing
    
    * Finished
    
    * Snapshot written
    
    * Make progress not rounded
    
    * added remaining time progressbar in Notification
    
    * added remaining time progressbar in Notification
    
    * Ran tests

---
+++ docs/pages/components/notification/examples/ExAutoCloseWithProgressBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <b-button
+            label="Show"
+            :disabled="isActive"
+            @click="isActive = true" />
+        <b-notification 
+            auto-close 
+            :progress-bar='progressBar' 
+            :duration="duration" 
+            title="Error!" 
+            type="is-dark" 
+            has-icon 
+            v-model="isActive" 
+            aria-close-label="Close notification">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+        </b-notification>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isActive: false,
+                duration: 4000,
+                progressBar: true
+            }
+        }
+    }
+</script>
