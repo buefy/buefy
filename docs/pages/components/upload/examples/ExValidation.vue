9dbdb1e683802318912a7b614358de30563c5348
l1bra2013 <92163989+l1bra2013@users.noreply.github.com>
2021-11-30 13:48:58 -0500

    fix upload validation bugs

---
+++ docs/pages/components/upload/examples/ExValidation.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-field class="file is-primary" :class="{'has-name': !!file}">
+        <b-upload v-model="file" class="file-label" accept=".tar.gz" required validationMessage="Please select a file">
+            <span class="file-cta">
+                <b-icon class="file-icon" icon="upload"></b-icon>
+                <span class="file-label">Click to upload (Only .tar.gz)</span>
+            </span>
+            <span class="file-name" v-if="file">
+                {{ file.name }}
+            </span>
+        </b-upload>
+    </b-field>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                file: null,
+            }
+        }
+    }
+</script>
