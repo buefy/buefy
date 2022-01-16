a4969ac33a52a60c5125549d98b7d8031d139570
chadlafontaine <96788334+chadlafontaine@users.noreply.github.com>
2022-01-16 11:49:13 -0600

    Added the page input to the b-pagination and the b-table that allows the user to enter a specific page. It also allows the developer to specify where the page input should be and specify a debounce time.

--- src/components/pagination/Pagination.vue
+++ src/components/pagination/Pagination.vue
@@ -3,60 +3,76 @@
         <slot
             v-if="$scopedSlots.previous"
             name="previous"
-            :page="getPage(current - 1, {
-                disabled: !hasPrev,
-                class: 'pagination-previous',
-                'aria-label': ariaPreviousLabel
-        })">
+            :page="
+                getPage(current - 1, {
+                    disabled: !hasPrev,
+                    class: 'pagination-previous',
+                    'aria-label': ariaPreviousLabel,
+                })
+            "
+        >
             <b-icon
                 :icon="iconPrev"
                 :pack="iconPack"
                 both
-                aria-hidden="true"/>
+                aria-hidden="true" />
         </slot>
         <BPaginationButton
             v-else
             class="pagination-previous"
             :disabled="!hasPrev"
             :page="getPage(current - 1)"
-            :aria-label="ariaPreviousLabel">
+            :aria-label="ariaPreviousLabel"
+        >
             <b-icon
                 :icon="iconPrev"
                 :pack="iconPack"
                 both
-                aria-hidden="true"/>
+                aria-hidden="true" />
         </BPaginationButton>
         <slot
             v-if="$scopedSlots.next"
             name="next"
-            :page="getPage(current + 1, {
-                disabled: !hasNext,
-                class: 'pagination-next',
-                'aria-label': ariaNextLabel
-        })">
+            :page="
+                getPage(current + 1, {
+                    disabled: !hasNext,
+                    class: 'pagination-next',
+                    'aria-label': ariaNextLabel,
+                })
+            "
+        >
             <b-icon
                 :icon="iconNext"
                 :pack="iconPack"
                 both
-                aria-hidden="true"/>
+                aria-hidden="true" />
         </slot>
         <BPaginationButton
             v-else
             class="pagination-next"
             :disabled="!hasNext"
             :page="getPage(current + 1)"
-            :aria-label="ariaNextLabel">
+            :aria-label="ariaNextLabel"
+        >
             <b-icon
                 :icon="iconNext"
                 :pack="iconPack"
                 both
-                aria-hidden="true"/>
+                aria-hidden="true" />
         </BPaginationButton>
 
+        <b-numberinput
+            v-if="pageInput"
+            class="pagination-input"
+            min="1"
+            :max="pageCount"
+            :value="inputValue"
+            :controls="false"
+            @input.native="onInput"
+        />
+
         <small class="info" v-if="simple">
-            <template v-if="perPage == 1">
-                {{ firstItem }} / {{ total }}
-            </template>
+            <template v-if="perPage == 1"> {{ firstItem }} / {{ total }} </template>
             <template v-else>
                 {{ firstItem }}-{{ Math.min(current * perPage, total) }} / {{ total }}
             </template>
@@ -64,37 +80,26 @@
         <ul class="pagination-list" v-else>
             <!--First-->
             <li v-if="hasFirst">
-                <slot
-                    v-if="$scopedSlots.default"
-                    :page="getPage(1)"
-                />
-                <BPaginationButton
-                    v-else
-                    :page="getPage(1)" />
+                <slot v-if="$scopedSlots.default" :page="getPage(1)" />
+                <BPaginationButton v-else :page="getPage(1)" />
             </li>
-            <li v-if="hasFirstEllipsis"><span class="pagination-ellipsis">&hellip;</span></li>
+            <li v-if="hasFirstEllipsis">
+                <span class="pagination-ellipsis">&hellip;</span>
+            </li>
 
             <!--Pages-->
             <li v-for="page in pagesInRange" :key="page.number">
-                <slot
-                    v-if="$scopedSlots.default"
-                    :page="page"
-                />
-                <BPaginationButton
-                    v-else
-                    :page="page" />
+                <slot v-if="$scopedSlots.default" :page="page" />
+                <BPaginationButton v-else :page="page" />
             </li>
 
             <!--Last-->
-            <li v-if="hasLastEllipsis"><span class="pagination-ellipsis">&hellip;</span></li>
+            <li v-if="hasLastEllipsis">
+                <span class="pagination-ellipsis">&hellip;</span>
+            </li>
             <li v-if="hasLast">
-                <slot
-                    v-if="$scopedSlots.default"
-                    :page="getPage(pageCount)"
-                />
-                <BPaginationButton
-                    v-else
-                    :page="getPage(pageCount)" />
+                <slot v-if="$scopedSlots.default" :page="getPage(pageCount)" />
+                <BPaginationButton v-else :page="getPage(pageCount)" />
             </li>
         </ul>
     </nav>
@@ -104,12 +109,15 @@
 import PaginationButton from './PaginationButton'
 import Icon from '../icon/Icon'
 import config from '../../utils/config'
+import NumberInput from '../numberinput/Numberinput'
+import debounce from '../../utils/debounce'
 
 export default {
     name: 'BPagination',
     components: {
         [Icon.name]: Icon,
-        [PaginationButton.name]: PaginationButton
+        [PaginationButton.name]: PaginationButton,
+        [NumberInput.name]: NumberInput
     },
     // deprecated, to replace with default 'value' in the next breaking change
     model: {
@@ -154,16 +162,29 @@ export default {
         ariaNextLabel: String,
         ariaPreviousLabel: String,
         ariaPageLabel: String,
-        ariaCurrentLabel: String
+        ariaCurrentLabel: String,
+        pageInput: {
+            type: Boolean,
+            default: false
+        },
+        pageInputPosition: String,
+        debouncePageInput: Number
+    },
+    data() {
+        return {
+            inputValue: this.current
+        }
     },
     computed: {
         rootClasses() {
             return [
                 this.order,
                 this.size,
+                this.pageInputPosition,
                 {
                     'is-simple': this.simple,
-                    'is-rounded': this.rounded
+                    'is-rounded': this.rounded,
+                    'has-input': this.pageInput
                 }
             ]
         },
@@ -177,66 +198,66 @@ export default {
         },
 
         /**
-        * Total page size (count).
-        */
+     * Total page size (count).
+     */
         pageCount() {
             return Math.ceil(this.total / this.perPage)
         },
 
         /**
-        * First item of the page (count).
-        */
+     * First item of the page (count).
+     */
         firstItem() {
             const firstItem = this.current * this.perPage - this.perPage + 1
             return firstItem >= 0 ? firstItem : 0
         },
 
         /**
-        * Check if previous button is available.
-        */
+     * Check if previous button is available.
+     */
         hasPrev() {
             return this.current > 1
         },
 
         /**
-        * Check if first page button should be visible.
-        */
+     * Check if first page button should be visible.
+     */
         hasFirst() {
-            return this.current >= (2 + this.beforeCurrent)
+            return this.current >= 2 + this.beforeCurrent
         },
 
         /**
-        * Check if first ellipsis should be visible.
-        */
+     * Check if first ellipsis should be visible.
+     */
         hasFirstEllipsis() {
-            return this.current >= (this.beforeCurrent + 4)
+            return this.current >= this.beforeCurrent + 4
         },
 
         /**
-        * Check if last page button should be visible.
-        */
+     * Check if last page button should be visible.
+     */
         hasLast() {
             return this.current <= this.pageCount - (1 + this.afterCurrent)
         },
 
         /**
-        * Check if last ellipsis should be visible.
-        */
+     * Check if last ellipsis should be visible.
+     */
         hasLastEllipsis() {
             return this.current < this.pageCount - (2 + this.afterCurrent)
         },
 
         /**
-        * Check if next button is available.
-        */
+     * Check if next button is available.
+     */
         hasNext() {
             return this.current < this.pageCount
         },
 
         /**
-        * Get near pages, 1 before and 1 after the current.
-        * Also add the click event to the array.
-        */
+     * Get near pages, 1 before and 1 after the current.
+     * Also add the click event to the array.
+     */
         pagesInRange() {
             if (this.simple) return
 
@@ -257,41 +278,56 @@ export default {
         }
     },
     watch: {
-        /**
-        * If current page is trying to be greater than page count, set to last.
-        */
+    /**
+     * If current page is trying to be greater than page count, set to last.
+     */
         pageCount(value) {
             if (this.current > value) this.last()
+        },
+
+        current(value) {
+            this.inputValue = value
+        },
+
+        debouncePageInput: {
+            handler(value) {
+                this.debounceHandlePageInput = debounce(
+                    this.handleInputPageChange,
+                    value
+                )
+            },
+            immediate: true
         }
     },
     methods: {
-        /**
-        * Previous button click listener.
-        */
+    /**
+     * Previous button click listener.
+     */
         prev(event) {
             this.changePage(this.current - 1, event)
         },
         /**
-        * Next button click listener.
-        */
+     * Next button click listener.
+     */
         next(event) {
             this.changePage(this.current + 1, event)
         },
         /**
-        * First button click listener.
-        */
+     * First button click listener.
+     */
         first(event) {
             this.changePage(1, event)
         },
         /**
-        * Last button click listener.
-        */
+     * Last button click listener.
+     */
         last(event) {
             this.changePage(this.pageCount, event)
         },
 
         changePage(num, event) {
             if (this.current === num || num < 1 || num > this.pageCount) return
+
             this.$emit('update:current', num)
             this.$emit('change', num)
 
@@ -306,22 +342,46 @@ export default {
                 number: num,
                 isCurrent: this.current === num,
                 click: (event) => this.changePage(num, event),
+                input: (event, inputNum) => this.changePage(+inputNum, event),
                 disabled: options.disabled || false,
                 class: options.class || '',
-                'aria-label': options['aria-label'] || this.getAriaPageLabel(num, this.current === num)
+                'aria-label':
+          options['aria-label'] ||
+          this.getAriaPageLabel(num, this.current === num)
             }
         },
 
         /**
-        * Get text for aria-label according to page number.
-        */
+     * Get text for aria-label according to page number.
+     */
         getAriaPageLabel(pageNumber, isCurrent) {
             if (this.ariaPageLabel && (!isCurrent || !this.ariaCurrentLabel)) {
                 return this.ariaPageLabel + ' ' + pageNumber + '.'
             } else if (this.ariaPageLabel && isCurrent && this.ariaCurrentLabel) {
-                return this.ariaCurrentLabel + ', ' + this.ariaPageLabel + ' ' + pageNumber + '.'
+                return (
+                    this.ariaCurrentLabel +
+          ', ' +
+          this.ariaPageLabel +
+          ' ' +
+          pageNumber +
+          '.'
+                )
             }
             return null
+        },
+
+        handleInputPageChange(event) {
+            this.getPage(this.inputValue).input(event, this.inputValue)
+        },
+
+        onInput(event) {
+            this.inputValue = +event.target.value
+
+            if (this.debouncePageInput) {
+                this.debounceHandlePageInput(event)
+            } else {
+                this.handleInputPageChange(event)
+            }
         }
     }
 }
