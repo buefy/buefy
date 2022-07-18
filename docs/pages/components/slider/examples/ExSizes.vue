0562b9d407e0fc2f6dffedcaface746abad0d303
Kikuo Emoto <kemoto@codemonger.io>
2022-07-18 17:24:00 +0900

    chore(docs): update Slider
    - Updates the documentation page of `Slider`. `:value` is replaced with
      `:model-value`.

--- docs/pages/components/slider/examples/ExSizes.vue
+++ docs/pages/components/slider/examples/ExSizes.vue
@@ -1,22 +1,22 @@
 <template>
     <section>
         <b-field label="Small">
-            <b-slider size="is-small" :value="20">
+            <b-slider size="is-small" :model-value="20">
             </b-slider>
         </b-field>
 
         <b-field label="Default">
-            <b-slider :value="20">
+            <b-slider :model-value="20">
             </b-slider>
         </b-field>
 
         <b-field label="Medium">
-            <b-slider size="is-medium" :value="20">
+            <b-slider size="is-medium" :model-value="20">
             </b-slider>
         </b-field>
 
         <b-field label="Large">
-            <b-slider size="is-large" :value="20">
+            <b-slider size="is-large" :model-value="20">
             </b-slider>
         </b-field>
     </section>
