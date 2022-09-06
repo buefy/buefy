b396c5d92a845031f58cdc77c3aa71539dbbf1af
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2022-09-06 15:02:07 +0200

    Implements disableLongPress property in the Numberinput component

--- src/components/numberinput/Numberinput.vue
+++ src/components/numberinput/Numberinput.vue
@@ -143,7 +143,11 @@ export default {
         controlsPosition: String,
         placeholder: [Number, String],
         ariaMinusLabel: String,
-        ariaPlusLabel: String
+        ariaPlusLabel: String,
+        disableLongPress: {
+            type: Boolean,
+            default: false
+        }
     },
     data() {
         return {
@@ -301,6 +305,7 @@ export default {
             }, this.exponential ? (250 / (this.exponential * this.timesPressed++)) : 250)
         },
         onStartLongPress(event, inc) {
+            if (this.disableLongPress) return
             if (event.button !== 0 && event.type !== 'touchstart') return
             clearTimeout(this._$intervalRef)
             this.longPressTick(inc)
