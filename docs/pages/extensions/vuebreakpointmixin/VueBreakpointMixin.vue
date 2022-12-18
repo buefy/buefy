802e47aafccab230c7fe463a19933f07be8c0fff
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-17 19:44:45 -0600

    feat: 🎸 Adding new page for VueBreakpointMixin

---
+++ docs/pages/extensions/vuebreakpointmixin/VueBreakpointMixin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <div class="buttons">
+          <a
+              class="button"
+              href="https://www.npmjs.com/package/vue-breakpoint-mixin"
+              target="_blank">
+              Website
+          </a>
+      </div>
+
+      <b-message type="is-info">
+          <p><strong>VueBreakpointMixin is not included in Buefy</strong>, you have to install it:</p>
+          <CodeView code="npm install vue-breakpoint-mixin" expanded/>
+      </b-message>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueBreakpointMixin',
+}
+</script>
