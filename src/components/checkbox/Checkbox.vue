144c5cc28f765a2c9b5f3c296f0c5d90029bd7b8
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-02-08 12:04:14 -0600

    feat: 🎸 Adding a new prop to set the id for inner input
    Adding a new prop to set the id for inner input for checkbox component

--- src/components/checkbox/Checkbox.vue
+++ src/components/checkbox/Checkbox.vue
@@ -12,6 +12,7 @@
              trigger the checkbox change in the b-table -->
         <input
             v-model="computedValue"
+            :id="innerInputId"
             :indeterminate.prop="indeterminate"
             type="checkbox"
             ref="input"
@@ -49,6 +50,10 @@ export default {
         autocomplete: {
             type: String,
             default: 'on'
+        },
+        innerInputId: {
+            type: String,
+            default: ''
         }
     }
 }
