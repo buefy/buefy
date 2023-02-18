6d15670cd1f936d2c7cdf319853f63b3863a3a43
Rajneesh <contact@scriptburn.com>
2023-02-18 18:16:58 +0530

    Update VeeValidate.vue
    Fixing links throwing 404 error

--- docs/pages/extensions/veevalidate/VeeValidate.vue
+++ docs/pages/extensions/veevalidate/VeeValidate.vue
@@ -3,19 +3,19 @@
         <div class="buttons">
             <a
                 class="button"
-                href="https://logaretm.github.io/vee-validate/"
+                href="https://vee-validate.logaretm.com/v3"
                 target="_blank">
                 Website
             </a>
             <a
                 class="button"
-                href="https://logaretm.github.io/vee-validate/guide/basics.html"
+                href="https://vee-validate.logaretm.com/v3/guide/basics.html"
                 target="_blank">
                 Docs
             </a>
             <a
                 class="button"
-                href="https://codesandbox.io/s/veevalidate-components-buefy-znj4u"
+                href="https://vee-validate.logaretm.com/v3/guide/3rd-party-libraries.html#buefy"
                 target="_blank">
                 Example
             </a>
