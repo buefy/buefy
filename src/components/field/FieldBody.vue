304a494680453546544db6257ae90ba844e05460
Kikuo Emoto <kemoto@codemonger.io>
2023-07-20 12:31:18 +0900

    fix(lib): performance warning on FieldBody
    - Removes a Vue runtime warning that indicated a potential performance
      issue related to how `FieldBody` gave the default slot to `Field`.
      Changes the third parameter of `h` function call to create a `Field`
      from a direct child `[element]` to a function slot `() => element`.
    
      This will address the issue pointed at in the comment:
      https://github.com/ntohq/buefy/issues/1#issuecomment-1643034295

--- src/components/field/FieldBody.vue
+++ src/components/field/FieldBody.vue
@@ -40,7 +40,7 @@ export default {
                                 type: this.type,
                                 message
                             },
-                            [element]
+                            () => element
                         )
                     })
                 }
