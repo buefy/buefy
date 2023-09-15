e70e8edfd6d936337a91639037932f0bc13b9d06
Kikuo Emoto <kemoto@codemonger.io>
2023-09-16 06:06:55 +0900

    Port spec for Timepicker (#72)
    * test(lib): update Timepicker spec
    
    - Updates the unit tests (spec) of `Timepicker` so that they can work
      with Vue 3 and `@vue/test-utils` V2:
        - Prevents the following components from becoming stubs because
          default slots in a stub are no longer rendered on
          `@vue/test-utils` V2:
            - `Dropdown`
            - `DropdownItem`
            - `Field`
            - `Select`
    
          The snapshot and test case "disables hour if no selectable minutes
          available" depend on slots in `Select`.
        - Updates expected values of "disabled" attribute, because Vue 3
          changed treatment of boolean attributes:
            - disabled: "disabled" → ""
            - not disabled: non "disabled" → undefined
        - Awaits `wrapper.setProps` to make sure the subsequent tests verify
          the updated state.
        - Replaces `wrapper.name()` with `wrapper.vm.$options.name` because
          `wrapper.name` no longer exists in `@vue/test-utils` V2.
        - Replaces the test on `wrapper.isVueInstance()` with presence of
          `wrapper.vm` because `wrapper.isVueInstance` no longer exists in
          `@vue/test-utils` V2.
        - Refreshes the snapshot. I did not find any problems in the updated
          snapshot.
    
    issue ntohq/buefy-next#1
    
    * test(lib): update TimepickerMixin spec
    
    - Updates the unit tests (spec) of `TimepickerMixin` so that they can
      work with `@vue/test-utils` V2:
        - Corrects a v-model binding:
            - event: `input` → `update:modelValue`
        - Moves the `mixins` option for `shallowMount` to the component's
          definition. Because it is more straightforward.
        - Replaces `attachToDocument` with `attachTo: document.body`,
          because `attachToDocument` might be deprecated in the future.
        - Replaces legacy async test cases with async functions.
    
    issue ntohq/buefy#1

--- src/components/timepicker/Timepicker.spec.js
+++ src/components/timepicker/Timepicker.spec.js
@@ -5,30 +5,44 @@ let wrapper
 
 describe('BTimepicker', () => {
     beforeEach(() => {
-        wrapper = shallowMount(BTimepicker)
+        wrapper = shallowMount(BTimepicker, {
+            global: {
+                stubs: {
+                    // these components must not be stubs
+                    // to reproduce a snapshot closer to the legacy one
+                    //
+                    // "disables hour if no selectable minutes available"
+                    // test case also depends on slots in <b-select>
+                    'b-dropdown': false,
+                    'b-dropdown-item': false,
+                    'b-field': false,
+                    'b-select': false
+                }
+            }
+        })
     })
 
     it('is called', () => {
-        expect(wrapper.name()).toBe('BTimepicker')
-        expect(wrapper.isVueInstance()).toBeTruthy()
+        expect(wrapper.vm).toBeTruthy()
+        expect(wrapper.vm.$options.name).toBe('BTimepicker')
     })
 
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
 
-    it('returns "1" for nativeStep is enableSeconds is true', () => {
+    it('returns "1" for nativeStep is enableSeconds is true', async () => {
         expect(wrapper.vm.nativeStep).toBeUndefined()
-        wrapper.setProps({ enableSeconds: true })
+        await wrapper.setProps({ enableSeconds: true })
         expect(wrapper.vm.nativeStep).toBe('1')
     })
 
-    it('disables hour if no selectable minutes available', () => {
-        wrapper.setProps({
-            'minTime': new Date(2019, 8, 22, 11, 55),
-            'incrementMinutes': 10
+    it('disables hour if no selectable minutes available', async () => {
+        await wrapper.setProps({
+            minTime: new Date(2019, 8, 22, 11, 55),
+            incrementMinutes: 10
         })
-        expect(wrapper.find('option[value="11"]').attributes()['disabled']).toBe('disabled')
-        expect(wrapper.find('option[value="12"]').attributes()['disabled']).not.toBe('disabled')
+        expect(wrapper.find('option[value="11"]').attributes().disabled).toBe('')
+        expect(wrapper.find('option[value="12"]').attributes().disabled).toBeUndefined()
     })
 })
--- src/utils/TimepickerMixin.spec.js
+++ src/utils/TimepickerMixin.spec.js
@@ -9,74 +9,62 @@ describe('TimepickerMixin', () => {
     HTMLElement.prototype.insertAdjacentElement = jest.fn()
     beforeEach(() => {
         const component = {
-            template: '<div class="b-component"></div>'
+            template: '<div class="b-component"></div>',
+            mixins: [TimepickerMixin]
         }
         wrapper = shallowMount(component, {
-            attachToDocument: true,
-            mixins: [TimepickerMixin]
+            attachTo: document.body
         })
     })
 
-    it('should set dateSelected and emit input event when setting computedValue', (done) => {
+    it('should set dateSelected and emit input event when setting computedValue', async () => {
         const date = new Date()
         wrapper.vm.computedValue = date
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.dateSelected).toEqual(date)
-            expect(wrapper.emitted()['input']).toBeTruthy()
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.dateSelected).toEqual(date)
+        expect(wrapper.emitted()['update:modelValue']).toBeTruthy()
     })
 
     it('should return correct value for meridiens', () => {
         expect(wrapper.vm.meridiens).toEqual([AM, PM])
     })
 
-    it('should call updateDateSelected on onSecondsChange', (done) => {
+    it('should call updateDateSelected on onSecondsChange', async () => {
         wrapper.vm.updateDateSelected = jest.fn()
         wrapper.vm.onSecondsChange()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.updateDateSelected).toHaveBeenCalledTimes(1)
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.updateDateSelected).toHaveBeenCalledTimes(1)
     })
 
-    it('should update internal state with passed value on updateInternalState', (done) => {
+    it('should update internal state with passed value on updateInternalState', async () => {
         const date = new Date()
         wrapper.vm.updateInternalState(date)
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.hoursSelected).toEqual(date.getHours())
-            expect(wrapper.vm.minutesSelected).toEqual(date.getMinutes())
-            expect(wrapper.vm.secondsSelected).toEqual(date.getSeconds())
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.hoursSelected).toEqual(date.getHours())
+        expect(wrapper.vm.minutesSelected).toEqual(date.getMinutes())
+        expect(wrapper.vm.secondsSelected).toEqual(date.getSeconds())
     })
 
-    it('should update internal state on updateInternalState', (done) => {
+    it('should update internal state on updateInternalState', async () => {
         wrapper.vm.updateInternalState()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.hoursSelected).toBeNull()
-            expect(wrapper.vm.minutesSelected).toBeNull()
-            expect(wrapper.vm.secondsSelected).toBeNull()
-            expect(wrapper.vm.meridienSelected).toEqual(AM)
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.hoursSelected).toBeNull()
+        expect(wrapper.vm.minutesSelected).toBeNull()
+        expect(wrapper.vm.secondsSelected).toBeNull()
+        expect(wrapper.vm.meridienSelected).toEqual(AM)
     })
 
-    it('should call onFocus on handleOnFocus', (done) => {
+    it('should call onFocus on handleOnFocus', async () => {
         wrapper.vm.onFocus = jest.fn()
         wrapper.vm.handleOnFocus()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.onFocus).toHaveBeenCalledTimes(1)
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.onFocus).toHaveBeenCalledTimes(1)
     })
 
-    it('should call toggle on close', (done) => {
+    it('should call toggle on close', async () => {
         wrapper.vm.toggle = jest.fn()
         wrapper.vm.close()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.toggle).toHaveBeenCalledTimes(1)
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.toggle).toHaveBeenCalledTimes(1)
     })
 })
