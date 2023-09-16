f14d4057fa7ea91959393c928ddd574db69b8e59
Kikuo Emoto <kemoto@codemonger.io>
2023-09-17 06:03:52 +0900

    test(lib): update Snackbar spec (#78)
    - Updates the unit tests (spec) of `Snackbar` so that they can work with
      `@vue/test-utils` V2:
        - Replaces `wrapper.name()` with `wrapper.vm.$options.name` because
          `wrapper.name` no longer exists in `@vue/test-utils` V2.
        - Replaces the test on `wrapper.isVueInstance()` with presence of
          `wrapper.vm` because `wrapper.isVueInstance` no longer exists in
          `@vue/test-utils` V2.
        - Replaces a legacy async test case with an async function.
        - Replaces `attachToDocument: true` option for `shallowMount` with
          `attachTo: document.body` because `attachToDocument` is no longer
          effective on `@vue/test-utils` V2.
        - Replaces `propsData` option for `shallowMount` with `props`
          because `propsData` might be deprecated in the future.
        - Refreshes the snapshot. I did not find any problems in the updated
          snapshot.
    
    issue ntohq/buefy-next#1

--- src/components/snackbar/Snackbar.spec.js
+++ src/components/snackbar/Snackbar.spec.js
@@ -7,29 +7,27 @@ describe('BSnackbar', () => {
     HTMLElement.prototype.insertAdjacentElement = jest.fn()
     beforeEach(() => {
         wrapper = shallowMount(BSnackbar, {
-            attachToDocument: true,
-            propsData: {
+            attachTo: document.body,
+            props: {
                 onAction: jest.fn()
             }
         })
     })
 
     it('is called', () => {
-        expect(wrapper.name()).toBe('BSnackbar')
-        expect(wrapper.isVueInstance()).toBeTruthy()
+        expect(wrapper.vm).toBeTruthy()
+        expect(wrapper.vm.$options.name).toBe('BSnackbar')
     })
 
     it('render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
 
-    it('calls the action listener and close on action', (done) => {
+    it('calls the action listener and close on action', async () => {
         wrapper.vm.close = jest.fn()
         wrapper.vm.action()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.onAction).toHaveBeenCalled()
-            expect(wrapper.vm.close).toHaveBeenCalled()
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.onAction).toHaveBeenCalled()
+        expect(wrapper.vm.close).toHaveBeenCalled()
     })
 })
