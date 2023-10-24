f3da8ac29918b2a4bc87619ea1df0b2411526986
Wesley Ford <wes@wesdev.pro>
2023-10-23 21:34:55 -0500

    fix(docs): updated all unchanged import paths

--- packages/docs/src/pages/components/colorpicker/examples/ExRepresentation.vue
+++ packages/docs/src/pages/components/colorpicker/examples/ExRepresentation.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import Color from '../../../../../../buefy-next/utils/color'
+import Color from '../../../../../../buefy-next/src/utils/color'
 
 export default {
     data() {
