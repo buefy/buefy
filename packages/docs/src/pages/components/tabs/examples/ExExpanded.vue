82a1260320296232e64fcae9d2080658a5ad26df
Wesley Ford <wes@wesdev.pro>
2023-10-25 13:24:51 -0500

    fix(docs): Change deprecated icons for `Tabs` components
    1. Changed `google-photos` icon to `camera-iris`
    2. Changed `library-music` icon to `music-box-multiple`

--- packages/docs/src/pages/components/tabs/examples/ExExpanded.vue
+++ packages/docs/src/pages/components/tabs/examples/ExExpanded.vue
@@ -1,7 +1,7 @@
 <template>
     <b-tabs type="is-toggle" expanded>
-        <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>
-        <b-tab-item label="Music" icon="library-music"></b-tab-item>
+        <b-tab-item label="Pictures" icon="camera-iris"></b-tab-item>
+        <b-tab-item label="Music" icon="music-box-multiple"></b-tab-item>
         <b-tab-item label="Videos" icon="video"></b-tab-item>
     </b-tabs>
 </template>
--- packages/docs/src/pages/components/tabs/examples/ExSizes.vue
+++ packages/docs/src/pages/components/tabs/examples/ExSizes.vue
@@ -1,19 +1,19 @@
 <template>
     <section>
         <b-tabs size="is-small" class="block">
-            <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>
+            <b-tab-item label="Pictures" icon="camera-iris"></b-tab-item>
             <b-tab-item label="Music" icon="library-music"></b-tab-item>
             <b-tab-item label="Videos" icon="video"></b-tab-item>
         </b-tabs>
 
         <b-tabs size="is-medium" class="block">
-            <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>
-            <b-tab-item label="Music" icon="library-music"></b-tab-item>
+            <b-tab-item label="Pictures" icon="camera-iris"></b-tab-item>
+            <b-tab-item label="Music" icon="music-box-multiple"></b-tab-item>
             <b-tab-item label="Videos" icon="video"></b-tab-item>
         </b-tabs>
 
         <b-tabs size="is-large" class="block">
-            <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>
+            <b-tab-item label="Pictures" icon="camera-iris"></b-tab-item>
             <b-tab-item label="Music" icon="library-music"></b-tab-item>
             <b-tab-item label="Videos" icon="video"></b-tab-item>
         </b-tabs>
