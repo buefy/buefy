62523721024be7c209945300f8ff5873abbf0861
Kikuo Emoto <kemoto@codemonger.io>
2023-10-31 11:25:08 +0900

    chore(lib): configure jest
    - Adds a new script `unit` that runs `jest` to perform unit tests. Also
      introduces a script `test` that runs `unit` and will also run ESLint
      later.
    - Installs dependencies necessary to run `jest`.
    - Updates `jest.config.js` because some configurations no longer work
      on newer `jest`.
    - Adds `vue^3.0.0` to `peerDependencies`.

--- packages/buefy-next/jest.config.js
+++ packages/buefy-next/jest.config.js
@@ -7,20 +7,27 @@ module.exports = {
         'vue'
     ],
     transform: {
-        '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-        '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+        '^.+\\.js$': 'babel-jest',
+        '.*\\.(vue)$': '@vue/vue3-jest'
     },
     moduleNameMapper: {
         '^@components/(.*)$': '<rootDir>/src/components/$1',
         '^@utils/(.*)$': '<rootDir>/src/utils/$1'
     },
     snapshotSerializers: [
-        '<rootDir>/node_modules/jest-serializer-vue'
+        'jest-serializer-vue'
     ],
     setupFiles: [
         './jest-setup.js'
     ],
     coverageDirectory: './coverage/',
     collectCoverage: true,
-    testURL: 'http://localhost/'
+    testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+        customExportConditions: [
+            'node',
+            'node-addons'
+        ],
+        url: 'http://localhost/'
+    }
 }
