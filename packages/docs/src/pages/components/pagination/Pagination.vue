0c782f5410fd2d50cd1d7511c9dd91972c84dd91
Kikuo Emoto <kemoto@codemonger.io>
2023-12-16 04:45:03 +0900

    Update custom pagination buttons example (buefy/buefy#3936)
    * fix(docs): custom pagination routing did not work
    
    - Fixes the issue that routing for "Custom pagination buttons" did not
      work. `ExSlot` tried to extract the page number from the hash in the
      current URL, though, `parseInt` always got an extra leading hash
      character ('#') in the input and ended up with `NaN`. Includes a hash
      character in the pattern for matching the non-digit part in the hash
      in the URL.
    
    issue buefy/buefy#3935
    
    * fix(docs): disabled page shown with custom pagination
    
    - Works around the problem that disabled "Previous" and "Next" buttons
      navigated to non-existing pages in Section "Custom pagination
      buttons". It happened when `router-link` was specified as custom
      pagination button tags. `BPaginationButton` tries to trap click events
      leading to disabled pages, however, `router-link` overwrites this
      behavior with its own click handler. As far as I investigated,
      `BPaginationButton` can do nothing to prevent this.
    
      A workaround I came up with is to switch `tag` between `a` and
      `router-link` according to `page.disabled` value in `v-slot`. Updates
      Section "Custom pagination buttons" to apply and remark this
      workaround.
    
    issue buefy/buefy#3852

--- packages/docs/src/pages/components/pagination/Pagination.vue
+++ packages/docs/src/pages/components/pagination/Pagination.vue
@@ -5,6 +5,9 @@
         <Example :component="ExSlot" :code="ExSlotCode" title="Custom pagination buttons">
             <p>You can customize the buttons using <b><code>b-pagination-button</code></b> as scoped slot.</p>
             <p>For example, you can customize the inner text or use <code>router-link</code> as tag.</p>
+            <b-message type="is-warning" has-icon>
+                You have to switch <code>tag</code> between <code>router-link</code> and <code>a</code>, or whichever tag you specify to <code>router-link</code>, according to <code>page.disabled</code> flag in the scoped slot props to prevent navigation to disabled or non-existing pages.
+            </b-message>
         </Example>
 
         <ApiView :data="api"/>
