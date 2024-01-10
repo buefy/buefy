879761c3b57d7220e554c577c1973e4a1cae1fcd
Kikuo Emoto <kemoto@codemonger.io>
2024-01-10 11:25:04 +0900

    fix(docs): grammatical errors in Dropdown
    - See comment:
      https://github.com/buefy/buefy/pull/3962#discussion_r1446808277
    
    Co-authored-by: Wesley Ford <wes@wesdevpro.com>

--- docs/pages/components/dropdown/Dropdown.vue
+++ docs/pages/components/dropdown/Dropdown.vue
@@ -2,8 +2,8 @@
     <div>
         <Example :component="ExSimple" :code="ExSimpleCode">
             <p>
-                It appears as a modal for tablets and smartphones, more precisely, when the screen is narrower than <code>$dropdown-mobile-breakpoint</code>.
-                However, Dropdowns with <code>"hover"</code> trigger won't change it's behavior to avoid any mulfunction with hover.
+                It appears as a modal for tablets and smartphones, more precisely when the screen is narrower than <code>$dropdown-mobile-breakpoint</code>.
+                However, dropdowns with <code>"hover"</code> trigger won't change its behavior to avoid any malfunction with hover.
                 <code>"hover"</code> trigger works like <code>"click"</code> trigger on touch devices where hover events do not make sense.
                 <code>"hover"</code> trigger precedes <code>"click"</code> trigger.
             </p>
