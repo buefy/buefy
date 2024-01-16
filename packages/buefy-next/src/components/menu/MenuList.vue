decc2c0e9fc94eef0715566892e86655c901ee82
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-01-16 04:10:21 +0100

    Feat: menu list as template (buefy/buefy#163)
    * Menu list as template
    
    The initial intention of this commit was to fix the problem that
    `MenuList` did not work in production, which had been fixed in
    another commit d44e6d20a3420efb1f91c092d6dd21fbf5481153.
    We make this change anyway because an ordinary component is
    more intuitive and easier to understand than a functional component.
    See https://github.com/ntohq/buefy-next/pull/163#issuecomment-1892072732

--- packages/buefy-next/src/components/menu/MenuList.vue
+++ packages/buefy-next/src/components/menu/MenuList.vue
@@ -1,51 +1,40 @@
+<template>
+    <p v-if="label || $slots.label" class="menu-label">
+        <template v-if="label">
+            <template v-if="icon">
+                <b-icon
+                    :icon="icon"
+                    :pack="iconPack"
+                    :size="size"
+                />
+                <span>{{ label }}</span>
+            </template>
+            <template v-else>
+                {{ label }}
+            </template>
+        </template>
+        <slot v-else name="label" />
+    </p>
+    <ul class="menu-list" :role="ariaRole === 'menu' ? ariaRole : undefined">
+        <slot />
+    </ul>
+</template>
+
 <script>
-import { h as createElement, resolveComponent } from 'vue'
-
-const BMenuList = (props, context) => {
-    let vlabel = null
-    const slots = context.slots
-    if (props.label || slots.label) {
-        vlabel = createElement(
-            'p',
-            { class: 'menu-label' },
-            props.label
-                ? props.icon
-                    ? [
-                        createElement(resolveComponent('b-icon'), {
-                            icon: props.icon,
-                            pack: props.iconPack,
-                            size: props.size
-                        }),
-                        createElement('span', {}, props.label)
-                    ]
-                    : props.label
-                : slots.label()
-        )
-    }
-    const vnode = createElement(
-        'ul',
-        {
-            class: 'menu-list',
-            role: props.ariaRole === 'menu' ? props.ariaRole : null
+export default {
+    name: 'BMenuList',
+    props: {
+        label: String,
+        icon: String,
+        iconPack: String,
+        ariaRole: {
+            type: String,
+            default: ''
         },
-        typeof slots.default === 'function' ? slots.default() : slots.default
-    )
-    return vlabel ? [vlabel, vnode] : vnode
-}
-
-BMenuList.props = {
-    label: String,
-    icon: String,
-    iconPack: String,
-    ariaRole: {
-        type: String,
-        default: ''
-    },
-    size: {
-        type: String,
-        default: 'is-small'
+        size: {
+            type: String,
+            default: 'is-small'
+        }
     }
 }
-
-export default BMenuList
 </script>
