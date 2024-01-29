792a755fba5c13574994b0af5e1785b96cd2934d
Kikuo Emoto <kemoto@codemonger.io>
2024-01-29 15:33:45 +0900

    feat(lib): rewrite utils/plugins in TS
    - Rewrites `utils/plugins.ts` in TypeScript, which was renamed from
      `.js` to `.ts` in the last commit.
    
      Augments global `Window` definition so that it can hold an optional
      Vue `App` instance, which is used by `use` function. However, as `use`
      no longer makes sense in Vue 3, this augmentation shall be removed
      when `use` is removed.
    
      Changes the signature of `use` so that it becomes equivalent to
      `App.use`. However, `use` shall be removed as mentioned above.
    
      Gives types to `registerComponent`.
    
      Gives types to `registerComponentProgrammatic`. `component` is typed
      as `any` for now, but must be refined in the future. `$buefy` is
      force initialized with an empty object to allow lazy registration.

--- packages/buefy-next/src/utils/plugins.ts
+++ packages/buefy-next/src/utils/plugins.ts
@@ -1,16 +1,35 @@
-export const use = (plugin) => {
-    if (typeof window !== 'undefined' && window.Vue) {
-        window.Vue.use(plugin)
+import type { App, Component, Plugin } from 'vue'
+
+// TODO: `use` shall be removed and this declaration shall also be removed.
+declare global {
+    interface Window {
+        Vue?: App
     }
 }
 
+// same signature as `App.use`: https://github.com/vuejs/core/blob/ee4cd78a06e6aa92b12564e527d131d1064c2cd0/packages/runtime-core/src/apiCreateApp.ts#L36-L39
+// TODO: `use` no longer makes sense in Vue 3.
+export const use: <T extends unknown[]>(plugin: Plugin<T>, ...options: T) => void =
+    (plugin, ...options) => {
+        if (typeof window !== 'undefined' && window.Vue) {
+            window.Vue.use(plugin, options)
+        }
+    }
+
 // use `name` to register a Functional Component which will become unresolvable
 // in production build due to name mangling.
-export const registerComponent = (Vue, component, name) => {
-    Vue.component(name || component.name, component)
+export const registerComponent = (Vue: App, component: Component, name?: string) => {
+    const componentName = name || component.name
+    if (componentName == null) {
+        throw new Error('Buefy.registerComponent: missing component name')
+    }
+    Vue.component(componentName, component)
 }
 
-export const registerComponentProgrammatic = (Vue, property, component) => {
-    if (!Vue.config.globalProperties.$buefy) Vue.config.globalProperties.$buefy = {}
+// TODO: refine the type of `component`.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const registerComponentProgrammatic = (Vue: App, property: string, component: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if (!Vue.config.globalProperties.$buefy) Vue.config.globalProperties.$buefy = {} as any
     Vue.config.globalProperties.$buefy[property] = component
 }
