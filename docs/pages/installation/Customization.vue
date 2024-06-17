9e57db99be670b51b283f873e363b85902bbd563
Dídac García <didac.garcia@promocionesfarma.com>
2024-06-17 12:21:03 +0200

    Fix: update link

--- docs/pages/installation/Customization.vue
+++ docs/pages/installation/Customization.vue
@@ -19,7 +19,7 @@
                 <h3 class="subtitle">Set your variables with Sass</h3>
                 <b-message type="is-info">
                     <div class="content">
-                        <p><b>Tip:</b> You can see <a href="https://web.archive.org/web/20240107060345/https://bulma.io/documentation/overview/variables/" target="_blank">all of Bulma's variables</a>.</p>
+                        <p><b>Tip:</b> You can see <a href="https://versions.bulma.io/0.9.4/documentation/customize/variables/" target="_blank">all of Bulma's variables</a>.</p>
                         <p>There are also two other variables created by Buefy:</p>
                         <ol>
                             <li><code>$speed-slow: 150ms !default</code></li>
