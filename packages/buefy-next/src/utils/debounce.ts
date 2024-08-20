6619d9a89e9915e3a0bf7f7d02d3021e7e801dc5
Kikuo Emoto <kemoto@codemonger.io>
2024-08-20 23:43:36 +0900

    feat(lib): rewrite debounce in TS
    Actually rewrites `utils/debounce.ts` in TypeScript:
    - Introduces two type parameters `This` and `Args`. `This` represents
      the `this` type (context) of the debounced function, and `Args`
      represents the spread arguments of the input function to be debounced
      so that the debounced function hasthe same parameter signature. You do
      not have to explicitly specify `Args` as it is inferred. However, if
      you want to limit the context of the debounced function, you have to
      explicitly give `This` like the following:
    
        debounce<Context>((...) => {/* this as Context */}, wait)
    
    - Removes `const args = arguments` in the inner function because
      `arguments` has `IArguments` which is different from `Args`. Instead,
      binds `args` to the rest parameters of the inner function. This should
      not change the behavior of the inner function.
    
    - Resets `timeout` with `undefined` instead of `null`, because
      `clearTimeout` does not accept `null`. Since `timeout` is only
      internally used, this change should not matter.
    
    - Trivial changes:
      - Gives other types
      - Suppresses unavoidable ESLint errors

--- packages/buefy-next/src/utils/debounce.ts
+++ packages/buefy-next/src/utils/debounce.ts
@@ -1,10 +1,15 @@
-export default function (func, wait, immediate) {
-    let timeout
-    return function () {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function <This, Args extends any[]> (
+    func: (this: This, ...args: Args) => unknown,
+    wait: number,
+    immediate?: boolean
+): (this: This, ...args: Args) => void {
+    let timeout: ReturnType<typeof setTimeout> | undefined
+    return function (this: This, ...args: Args) {
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
         const context = this
-        const args = arguments
         const later = function () {
-            timeout = null
+            timeout = undefined
             if (!immediate) func.apply(context, args)
         }
         const callNow = immediate && !timeout
