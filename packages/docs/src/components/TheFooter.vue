d458a6bd4a3de6c46ef5743a1eca5770f9909f67
Wesley Ford <wes@wesdevpro.com>
2025-08-10 19:54:29 -0400

    Update Buefy Footer (#4202)
    * chore: update buefy footer
    
    * chore: update navbar links
    
    * chore: fix linting issues

--- packages/docs/src/components/TheFooter.vue
+++ packages/docs/src/components/TheFooter.vue
@@ -1,89 +1,175 @@
 <template>
     <footer class="footer">
         <div class="container">
-            <div class="content">
-                <div class="columns">
-                    <div class="column">
+            <div class="columns is-multiline is-variable is-6">
+                <!-- Community -->
+                <div class="column is-one-third">
+                    <h5 class="title is-6">
+                        Community
+                    </h5>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://opencollective.com/buefy"
+                                class="navbar-item has-text-weight-semibold"
+                                target="_blank"
+                            >
+                                Buefy Open Collective
+                            </a>
+                        </li>
+                        <li>
+                            <router-link
+                                to="/events"
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                Events & Meetups
+                            </router-link>
+                        </li>
+                        <li>
+                            <a
+                                href="https://github.com/buefy/buefy/blob/dev/.github/CONTRIBUTING.md"
+                                class="navbar-item has-text-weight-semibold"
+                                target="_blank"
+                            >
+                                Contribute
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Resources -->
+                <div class="column is-one-third">
+                    <h5 class="title is-6">
+                        Resources
+                    </h5>
+                    <ul>
+                        <li>
+                            <router-link
+                                to="/"
+                                exact
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                Home
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link
+                                to="/documentation"
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                Documentation
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link
+                                to="/documentation/start#stackblitz"
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                StackBlitz Examples
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link
+                                to="/expo"
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                Expo
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Partners -->
+                <div class="column is-one-third">
+                    <h5 class="title is-6">
+                        Partners
+                    </h5>
+                    <ul>
+                        <li>
+                            <router-link
+                                to="/sponsors"
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                Sponsors
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link
+                                to="/support"
+                                class="navbar-item has-text-weight-semibold"
+                            >
+                                Get Support
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Social & Copyright -->
+            <div class="level mt-5 is-size-7">
+                <!-- Left: Copyright & Hosting -->
+                <div class="level-left has-text-centered-mobile">
+                    <div class="level-item">
                         <p>
-                            <small>
-                                Buefy's source code is licensed under
-                                <a
-                                    href="http://opensource.org/licenses/mit-license.php"
-                                    target="_blank"
-                                >MIT</a>.
-                            </small>
+                            Buefy © 2017–2025
+                            <a
+                                href="http://opensource.org/licenses/mit-license.php"
+                                class="has-text-primary"
+                                target="_blank"
+                            >
+                                MIT License
+                            </a><br>
                         </p>
-
-                        <b-field>
-                            <p class="control">
-                                <a
-                                    class="button"
-                                    href="https://github.com/buefy/buefy"
-                                    target="_blank"
-                                >
-                                    <b-icon icon="github" />
-                                    <span>GitHub</span>
-                                </a>
-                            </p>
-                            <p class="control">
-                                <a
-                                    class="button is-discord"
-                                    href="https://discord.buefy.org"
-                                    target="_blank"
-                                >
-                                    <b-icon icon="discord" />
-                                    <span>Discord</span>
-                                </a>
-                            </p>
-                            <p class="control">
-                                <a
-                                    class="button is-twitter"
-                                    href="https://twitter.com/buefycss"
-                                    target="_blank"
-                                >
-                                    <b-icon
-                                        pack="fa"
-                                        custom-class="fa-brands"
-                                        icon="x-twitter"
-                                    />
-                                </a>
-                            </p>
-                        </b-field>
-                        <small>
-                            This site is powered by
-                            <a href="https://www.netlify.com">Netlify</a>
-                        </small>
                     </div>
-
-                    <div class="column">
-                        <router-link
-                            to="/"
-                            exact
-                            class="navbar-item has-text-weight-semibold"
+                </div>
+                <div class="level-item has-text-centered">
+                    <p>
+                        Website content licensed
+                        <a
+                            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+                            class="has-text-primary"
+                            target="_blank"
                         >
-                            Home
-                        </router-link>
-
-                        <router-link
-                            to="/documentation"
-                            class="navbar-item has-text-weight-semibold"
+                            CC BY-NC-SA 4.0
+                        </a><br>
+                        Powered by
+                        <a
+                            href="https://www.netlify.com"
+                            class="has-text-primary"
                         >
-                            Documentation
-                        </router-link>
+                            Netlify
+                        </a>
+                    </p>
+                </div>
 
-                        <router-link
-                            to="/extensions"
-                            class="navbar-item has-text-weight-semibold"
+                <!-- Right: Social Icons -->
+                <div class="level-right has-text-centered-mobile">
+                    <div class="level-item">
+                        <a
+                            class="button is-small"
+                            href="https://github.com/buefy/buefy"
+                            target="_blank"
                         >
-                            Extensions
-                        </router-link>
-
-                        <router-link
-                            to="/expo"
-                            class="navbar-item has-text-weight-semibold"
+                            <b-icon icon="github" />
+                        </a>
+                        <a
+                            class="button is-small is-discord ml-1"
+                            href="https://discord.buefy.org"
+                            target="_blank"
                         >
-                            Expo
-                        </router-link>
+                            <b-icon icon="discord" />
+                        </a>
+                        <a
+                            class="button is-small is-twitter ml-1"
+                            href="https://twitter.com/buefycss"
+                            target="_blank"
+                        >
+                            <b-icon
+                                pack="fa"
+                                custom-class="fa-brands"
+                                icon="x-twitter"
+                            />
+                        </a>
                     </div>
                 </div>
             </div>
@@ -92,5 +178,5 @@
 </template>
 
 <script setup lang="ts">
-import { BField, BIcon } from 'buefy'
+import { BIcon } from 'buefy'
 </script>
