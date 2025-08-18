31d60c1f723be6726fadd81f307587882a8a5006
Wesley Ford <wes@wesdevpro.com>
2025-08-18 00:07:22 -0400

    chore: initial creation of tree

---
+++ packages/buefy/src/components/tree/Tree.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BTree',
+})
+</script>
---
+++ packages/buefy/src/components/tree/TreeNode.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+    name: 'BTreeNode',
+})
+</script>
