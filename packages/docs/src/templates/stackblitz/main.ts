ca140c2e1b9951a77db277a5eeac7ac5ad1a5acb
Wesley Ford <wes@wesdevpro.com>
2025-08-19 01:52:39 -0400

    fix: remove extra font import

--- packages/docs/src/templates/stackblitz/main.ts
+++ packages/docs/src/templates/stackblitz/main.ts
@@ -3,14 +3,8 @@ import { createApp } from 'vue'
 // Import Buefy
 import Buefy from 'buefy'
 
-
 // Import Buefy Styles
 import './styles/buefy.scss'
-
-// Import ICON Librarys
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@mdi/font/css/materialdesignicons.min.css'
-
 
 // Import ICON Librarys
 import '@fortawesome/fontawesome-free/css/all.css'
