c08dcac2bef477d483183f059639dea76ec56663
Wesley Ford <wes@wesdevpro.com>
2025-09-29 20:04:54 -0400

    chore: fix lint issues

--- packages/docs/src/components/TheSidebar.vue
+++ packages/docs/src/components/TheSidebar.vue
@@ -4,7 +4,8 @@
             <sidebar-search />
             <template v-for="items in data" :key="items.category">
                 <p class="sidebar-label">
-                    <b-icon v-if="items.categoryIcon"
+                    <b-icon
+                        v-if="items.categoryIcon"
                         pack="fa"
                         :icon="items.categoryIcon"
                     />
