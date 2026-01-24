8d033f7e9881c77b6d4e3aa7d67c7d86c0b5d317
Matthias <58266364+xiaut@users.noreply.github.com>
2026-01-25 00:29:08 +0100

    Updated CDN URLs and usage example (#4294)

--- packages/docs/src/pages/installation/usage/start.ts
+++ packages/docs/src/pages/installation/usage/start.ts
@@ -52,27 +52,38 @@ export const importingCDNHtml = `
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://unpkg.com/buefy/dist/buefy.min.css">
-</head>
+    <link rel="stylesheet" href="https://unpkg.com/buefy/dist/css/buefy.min.css" />
 
+    <script src="https://unpkg.com/vue@3"></script>
+    <!-- Full bundle -->
+    <script src="https://unpkg.com/buefy/dist/buefy.min.js"></script>
+    <!-- Individual component -->
+    <script src="https://unpkg.com/buefy/dist/components/collapse"></script>
+</head>
 <body>
     <div id="app">
-        <!-- Buefy components goes here -->
+        <b-collapse :model-value="true" aria-id="contentIdForA11y1">
+            <template #trigger="props">
+                <b-button label="Toggle" />
+            </template>
+            <div class="notification">
+                This text should show and hide by clicking on the “Toggle” button.
+            </div>
+        </b-collapse>
     </div>
 
-    <script src="https://unpkg.com/vue@2"></\script>
-    <!-- Full bundle -->
-    <script src="https://unpkg.com/buefy/dist/buefy.min.js"></\script>
-
-    <!-- Individual components -->
-    <script src="https://unpkg.com/buefy/dist/components/table"></\script>
-    <script src="https://unpkg.com/buefy/dist/components/input"></\script>
-
     <script>
-        new Vue({
-            el: '#app'
-        })
-    </\script>
+        const { createApp } = Vue;
+        // From the full bundle
+        const { BButton } = Buefy;
+        // For an individual component
+        const { BCollapse } = Collapse;
+
+        const app = createApp({
+            components: { BButton, BCollapse }
+        });
+        app.mount("#app");
+    </script>
 </body>
 </html>`
 
